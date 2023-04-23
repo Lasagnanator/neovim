daeced34667cbe84b30cbfbc6c2dc652176618dd
Lasagnanator <d96a07@gmail.com>
2023-04-23 21:03:55 +0200

    Updated bufferline, needs to be changed

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -12,16 +12,27 @@ bufferline.setup({
         numbers = "none",
         indicator = {
             style = "none",
-            -- icon = "i", -- set style to "icon", otherwise this is pointless
         },
         max_name_length = 20,
         max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
         tab_size = 20,
         diagnostics = "nvim_lsp", -- false or "nvim_lsp"
         diagnostics_update_in_insert = false,
-        diagnostics_indicator = function(count, level)
-            local icon = level:match("error") and " " or " " -- Equivalent to a if with the match as a condition
-            return " " .. count .. icon
+        -- diagnostics_indicator = function(count, level)
+        --     local icon = level:match("error") and " " or " " -- Equivalent to a if with the match as a condition
+        --     return " " .. count .. icon
+---@diagnostic disable-next-line: unused-local
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                or (e == "warning" and " " or "" )
+                s = s .. n .. sym
+            end
+            return s
+        end,
+        name_formatter = function (buf)
+            return " " .. buf.name
         end,
         custom_filter = function (buffer_number)
             local ft = vim.bo[buffer_number].filetype
@@ -46,20 +57,21 @@ bufferline.setup({
                 text = "Explorer",
                 highlight = "TabLineSel",
                 text_align = "center",
-                separator = false,
+                separator = true,
             },
         },
         color_icons = true, -- whether or not to add the filetype icon highlights
         show_buffer_icons = true, -- disable filetype icons for buffers
         show_buffer_close_icons = false,
         -- show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
-        get_element_icon = function(buf) return require('nvim-web-devicons').get_icon(buf, {default = false}) end,
+        -- get_element_icon = function(buf) return require('nvim-web-devicons').get_icon(buf, {default = false}) end,
+        -- buffer_close_icon = '',
         show_close_icon = false,
-        show_tab_indicators = false,
+        -- show_tab_indicators = true,
         separator_style = { "", "" },
         enforce_regular_tabs = false,
         always_show_bufferline = true,
-        hover = { enabled = false },
+        hover = { enabled = true, reveal = { "close" } },
         sort_by = "tabs",
     }
 })
