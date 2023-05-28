31b0f5d6210fb85a1dfe33059b329b7c5cfb342d
Lasagnanator <d96a07@gmail.com>
2023-05-28 16:12:01 +0200

    Reworked remaps to work in every mode

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -14,59 +14,61 @@ keymap( "", "<Esc>",   "<Esc>", silent )
 
 --<< ALL MODES >>--
 
+--<< Base
+keymap( "",           "<Leader>w", "<Cmd>w<CR>",                  silent )
+keymap( "",           "<Leader>q", "<Cmd>q!<CR>",                 silent )
+keymap( "",           "<Leader>W", "<Cmd>wa<CR>",                 silent )
+keymap( "",           "<Leader>Q", "<Cmd>qa!<CR>",                silent )
+keymap( "",           "<Leader>Z", "<Cmd>wa<CR>\
+                                    <Cmd>qa<CR>",                 silent )
+keymap( "",           "<Leader>r", "<Cmd>w<CR>\
+                                    <Cmd>e<CR>",                  silent )
+keymap( "",           "<Leader>@", "<Cmd>set cursorcolumn!<CR>",  silent )
+keymap( "",           "<A-s>",     ":saveas ",                    nosilent )
+
 --<< Yank and paste
-keymap( { "n", "x" }, "<Leader>y", [["+y]], silent )
-keymap( { "n", "x" }, "<Leader>p", [["+p]], silent )
-keymap( { "n", "x" }, "<Leader>P", [["+P]], silent )
-keymap( { "n", "x" }, "<Leader>d", [["+d]], silent )
-keymap( { "n", "x" }, "<Leader>D", [["+D]], silent )
+keymap( { "n", "x" }, "<Leader>y", [["+y]],                       silent )
+keymap( { "n", "x" }, "<Leader>p", [["+p]],                       silent )
+keymap( { "n", "x" }, "<Leader>P", [["+P]],                       silent )
+keymap( { "n", "x" }, "<Leader>d", [["+d]],                       silent )
+keymap( { "n", "x" }, "<Leader>D", [["+D]],                       silent )
+
+--<< Move between windows
+keymap( "",           "<A-h>",     "<Cmd>wincmd h<CR>",           silent )
+keymap( "",           "<A-j>",     "<Cmd>wincmd j<CR>",           silent )
+keymap( "",           "<A-k>",     "<Cmd>wincmd k<CR>",           silent )
+keymap( "",           "<A-l>",     "<Cmd>wincmd l<CR>",           silent )
+
+--<< Move windows                       
+keymap( "",           "<A-H>",     "<Cmd>wincmd H<CR>",           silent )
+keymap( "",           "<A-J>",     "<Cmd>wincmd J<CR>",           silent )
+keymap( "",           "<A-K>",     "<Cmd>wincmd K<CR>",           silent )
+keymap( "",           "<A-L>",     "<Cmd>wincmd L<CR>",           silent )
+
+--<< Resize windows
+keymap( "",           "<C-Left>",  "<Cmd>vertical resize -1<CR>", silent )
+keymap( "",           "<C-Right>", "<Cmd>vertical resize +1<CR>", silent )
+keymap( "",           "<C-Up>",    "<Cmd>resize +1<CR>",          silent )
+keymap( "",           "<C-Down>",  "<Cmd>resize -1<CR>",          silent )
+
+--<< Tabs
+keymap( "",           "<Leader>.", "<Cmd>tabnew<CR>",             silent )
+keymap( "",           "<Leader>,", "<Cmd>tabclose<CR>",           silent )
+keymap( "",           "<A-,>",     "<Cmd>tabprevious<CR>",        silent )
+keymap( "",           "<A-.>",     "<Cmd>tabnext<CR>",            silent )
+keymap( "",           "<A-<>",     "<Cmd>-tabmove<CR>",           silent )
+keymap( "",           "<A->>",     "<Cmd>+tabmove<CR>",           silent )
 
 
 --<< NORMAL MODE >>--
 
 --<< Base
-keymap( "n", "<Leader>w",     "<Cmd>w<CR>",                  silent )
-keymap( "n", "<Leader>q",     "<Cmd>q!<CR>",                 silent )
-keymap( "n", "<Leader>W",     "<Cmd>wa<CR>",                 silent )
-keymap( "n", "<Leader>Q",     "<Cmd>qa!<CR>",                silent )
-keymap( "n", "<Leader>Z",     "<Cmd>wa<CR>\
-                               <Cmd>qa<CR>",                 silent )
-keymap( "n", "<Leader>r",     "<Cmd>w<CR>\
-                               <Cmd>e<CR>",                  silent )
-keymap( "n", "<Leader>@",     "<Cmd>set cursorcolumn!<CR>",  silent )
-keymap( "n", "<A-s>",         ":saveas ",                    nosilent )
 keymap( "n", "<S-k>",         "k<S-j>",                      silent )
 
 --<< Window management
-keymap( "n", "<Leader>o",     "<Cmd>wincmd o<CR>",           silent )
+keymap( "n", "<Leader>`",     "<Cmd>wincmd o<CR>",           silent )
 keymap( "n", "<Leader><bar>", "<Cmd>wincmd <bar><CR>",       silent )
 keymap( "n", "<Leader>_",     "<Cmd>wincmd _<CR>",           silent )
-
---<< Move between windows
-keymap( "n", "<A-h>",         "<Cmd>wincmd h<CR>",           silent )
-keymap( "n", "<A-j>",         "<Cmd>wincmd j<CR>",           silent )
-keymap( "n", "<A-k>",         "<Cmd>wincmd k<CR>",           silent )
-keymap( "n", "<A-l>",         "<Cmd>wincmd l<CR>",           silent )
-
---<< Move windows                           
-keymap( "n", "<A-H>",         "<Cmd>wincmd H<CR>",           silent )
-keymap( "n", "<A-J>",         "<Cmd>wincmd J<CR>",           silent )
-keymap( "n", "<A-K>",         "<Cmd>wincmd K<CR>",           silent )
-keymap( "n", "<A-L>",         "<Cmd>wincmd L<CR>",           silent )
-
---<< Resize windows
-keymap( "n", "<C-Left>",      "<Cmd>vertical resize -1<CR>", silent )
-keymap( "n", "<C-Right>",     "<Cmd>vertical resize +1<CR>", silent )
-keymap( "n", "<C-Up>",        "<Cmd>resize +1<CR>",          silent )
-keymap( "n", "<C-Down>",      "<Cmd>resize -1<CR>",          silent )
-
---<< Tabs
-keymap( "n", "<Leader>.",     "<Cmd>tabnew<CR>",             silent )
-keymap( "n", "<Leader>,",     "<Cmd>tabclose<CR>",           silent )
-keymap( "n", "<A-,>",         "<Cmd>tabprevious<CR>",        silent )
-keymap( "n", "<A-.>",         "<Cmd>tabnext<CR>",            silent )
-keymap( "n", "<A-<>",         "<Cmd>-tabmove<CR>",           silent )
-keymap( "n", "<A->>",         "<Cmd>+tabmove<CR>",           silent )
 
 --<< Plugin
 keymap( "n", "<C-p>c",        "<Cmd>PackerCompile<CR>\
