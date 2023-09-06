1319f4034e9d0a8f1e285d9e0b256c76ff8803a5
Lasagnanator <d96a07@gmail.com>
2023-09-07 00:36:48 +0200

    Disabled Glow plugin setup in markdown file

--- lua/user/configs/markdown-plugins.lua
+++ lua/user/configs/markdown-plugins.lua
@@ -1,12 +1,12 @@
 --<< MARKDOWN PREVIEWERS >>--
 
 --<< Protected calls
-local ok, glow = pcall(require, "glow")
+-- local ok, glow = pcall(require, "glow")
 
 --<< Setup markdown-previewer
 vim.g.mkdp_filetypes = { "markdown" }
 
 --<< Setup Glow
-if ok then
-    glow.setup()
-end
+-- if ok then
+--     glow.setup()
+-- end
