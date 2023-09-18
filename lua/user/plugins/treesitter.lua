12bcf9480b18ff3f7f2fb9e9ba03419fea0f8086
Lasagnanator <d96a07@gmail.com>
2023-09-18 17:20:08 +0200

    Fixed treesitter initialization

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
     {
         "nvim-treesitter/nvim-treesitter", -- Syntax highlighting
         -- TODO: set priority
+        -- priority = 900,
+        main = "nvim-treesitter.configs",
         opts = {
             ensure_installed = { "lua", "bash" },
             highlight = {
@@ -9,10 +11,6 @@ return {
                 disable = { "yaml.ansible", "tex" },
             },
         },
-    },
-    {
-        "nvim-treesitter/playground", -- Treesitter informations in Neovim
-        lazy = true,
     },
     {
         "nvim-treesitter/nvim-treesitter-context", -- Shows which function are you editing on the first line of the screen
