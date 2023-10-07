2deae3a04c93e3f208453ba743780033d70617b1
Lasagnanator <d96a07@gmail.com>
2023-10-07 19:24:21 +0200

    UI customization
    Tabline and statusline configured through lualine.
    Dashboard entirely rewritten using Alpha.

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -27,182 +27,181 @@ return {
                         cond = require("lazy.status").has_updates,
                     },
                     "encoding",
-                    -- {
-                    --     "fileformat",
-                    --     symbols = {
-                    --         unix = " ", -- e712
-                    --         dos = " ", -- e70f
-                    --         mac = " ", -- e711
-                    --     },
-                    -- },
                     "filetype",
                 },
                 lualine_y = { "progress" },
                 lualine_z = { "location" }
             },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
-                lualine_y = {},
-                lualine_z = {}
-            },
+            tabline = {
+                lualine_a = {
+                    {
+                        "tabs",
+                        mode = 2,
+                        use_mode_colors = true,
+                        max_lenght = 20,
+                    }
+                },
+                lualine_y = {
+                    {
+                        "windows",
+                        use_mode_colors = true,
+                        max_lenght = 20,
+                        filetype_names = {},
+                        disabled_filetypes = {
+                            "NvimTree",
+                            "TelescopePrompt",
+                            "DressingInput",
+                            "Trouble",
+                            "mason",
+                            "packer",
+                            "help",
+                            "wiki",
+                            "DiffviewFiles",
+                            "qf",
+                            "toggleterm",
+                        },
+                    }
+                },
+            }
         },
-        dependencies = { "kyazdani42/nvim-web-devicons" },
+        dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     {
-        "nanozuki/tabby.nvim",
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            local theme = {
-                fill = 'TabLineFill',
-                head = 'lualine_a_visual',
-                current_tab = 'lualine_a_normal',
-                tab = 'lualine_b_normal',
-                current_win = 'lualine_a_normal',
-                win = 'lualine_b_normal',
-                tail = 'lualine_a_visual',
+            local ascii_name = "skull"
+            local ver = vim.version()
+            local alpha = require("alpha")
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "AlphaReady",
+                command = "set showtabline=0 | set laststatus=0"
+            })
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "AlphaClosed",
+                command = "set showtabline=2 | set laststatus=3"
+            })
+
+            local padding = {
+                type = "padding",
+                val = 2
             }
 
-            require('tabby.tabline').set(function(line)
-                return {
-                    {
-                        { ' 󰮯 ', hl = theme.head },
-                        line.sep('', theme.head, theme.fill),
-                    },
-                    line.tabs().foreach(function(tab)
-                        local hl = tab.is_current() and theme.current_tab or theme.tab
-                        return {
-                            line.sep('', hl, theme.fill),
-                            tab.is_current() and '' or '󰆣',
-                            tab.number(),
-                            tab.name(),
-                            line.sep('', hl, theme.fill),
-                            hl = hl,
-                            margin = ' ',
-                        }
-                    end),
-                    line.spacer(),
-                    line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-                        local hl = win.is_current() and theme.current_win or theme.tab
-                        return {
-                            line.sep('', hl, theme.fill),
-                            win.is_current() and '' or '',
-                            win.buf_name(),
-                            line.sep('', hl, theme.fill),
-                            hl = hl,
-                            margin = ' ',
-                        }
-                    end),
-                    {
-                        line.sep('', theme.tail, theme.fill),
-                        { '   ', hl = theme.tail },
-                    },
-                    hl = theme.fill,
+            local header = {
+                type = "text",
+                val = function()
+                    local text = {}
+                    local path = vim.fs.normalize(vim.fn.stdpath("config") .. "/nvim-headers/" .. ascii_name .. ".txt")
+                    for line in io.lines(path) do
+                        table.insert(text, line)
+                    end
+                    return text
+                end,
+                opts = {
+                    position = "center",
+                    hl = "Function"
                 }
-            end, {
-            buf_name = {
-                mode = "unique"
-            },
-        })
-        end,
-    },
-    {
-        "glepnir/dashboard-nvim", -- Customize the opening screen
-        opts = {
-            theme = "doom",
-            hide = {
-                statusline = true,
-                tabline = true,
-                winbar = true,
-            },
-            -- preview = {
-            --     command = "cat",
-            --     file_path = vim.fn.stdpath("config") .. "/nvim-headers/skull.txt",
-            --     file_height = 19,
-            --     file_width = 70,
-            -- },
-            config = {
-                header = {
-                    [[]],
-                    [[]],
-                    [[]],
-                    [[]],
-                    [[]],
-                    [[    .o oOOOOOOOo                                            0OOOo     ]],
-                    [[    Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO     ]],
-                    [[    OboO000000000000.OOo. .oOOOOOo.    OOOo.oOOOOOo..0000000000OO     ]],
-                    [[    OOP.oOOOOOOOOOOO 0POOOOOOOOOOOo.   `0OOOOOOOOOP,OOOOOOOOOOOB'     ]],
-                    [[    `O'OOOO'     `OOOOo"OOOOOOOOOOO` .adOOOOOOOOO"oOOO'    `OOOOo     ]],
-                    [[    .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO     ]],
-                    [[    OOOOO                 '"OOOOOOOOOOOOOOOO"`                oOO     ]],
-                    [[   oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.   ]],
-                    [[  oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO  ]],
-                    [[  OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"`  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO  ]],
-                    [[    :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?         .      ]],
-                    [[    Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`     ]],
-                    [[    .            oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo               ]],
-                    [[                 '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':                 ]],
-                    [[                      `$"  `OOOO' `O"Y ' `OOOO'  o             .      ]],
-                    [[    .                  .     OP"          : o     .                   ]],
-                    [[]],
-                    [[]],
-                    [[]],
-                    [[]],
-                    [[]],
+            }
+
+            local info = {
+                type = "group",
+                val = {
+                    {
+                        type = "text",
+                        val = "Gun revision " .. ver.major .. "." .. ver.minor .. "." .. ver.patch
+                    },
+                    {
+                        type = "text",
+                        val = ""
+                    },
+                    {
+                        type = "text",
+                        val = ""
+                    }
                 },
-                center = {
-                    {
-                        icon = "   ",
-                        desc = "New file",
-                        key = "n",
-                        action = "enew"
-                    },
-                    {
-                        icon = "   ",
-                        desc = "Restore last session",
-                        key = "l",
-                        action = "SessionManager load_last_session"
-                    },
-                    {
-                        icon = "   ",
-                        desc = "Restore directory session",
-                        key = "s",
-                        action = "SessionManager load_current_dir_session"
-                    },
-                    {
-                        icon = "   ",
-                        desc = "Search folder",
-                        key = "f",
-                        action = "Telescope find_files"
-                    },
-                    {
-                        icon = "   ",
-                        desc = "Find word",
-                        key = "w",
-                        action = "Telescope live_grep"
-                    },
-                    {
-                        icon = "   ",
-                        desc = "File browser",
-                        key = "e",
-                        action = "Telescope file_browser"
-                    },
-                    {
-                        icon = "   ",
-                        desc = "Quit Neovim",
-                        key = "q",
-                        action = ":q!"
-                    },
+                opts = {
+                    inherit = {
+                        position = "center",
+                        hl = "statement"
+                    }
+                }
+            }
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "LazyVimStarted",
+                callback = function()
+                    local stats = require("lazy").stats()
+                    local time = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                    alpha.default_config.layout[4].val[2].val = stats.loaded .. " bullets loaded out of " .. stats.count
+                    alpha.default_config.layout[4].val[3].val = "Machinegun ready in " .. time .. " ms"
+                    pcall(vim.cmd.AlphaRedraw)
+                end,
+            })
+
+            local function button(icon, label, shortcut, command)
+                return {
+                    type = "button",
+                    val = icon .. "   " .. label,
+                    on_press = function() vim.cmd(command) end,
+                    opts = {
+                        shortcut = string.upper(shortcut),
+                        keymap = { "n", shortcut, "<Cmd>" .. command .. "<CR>",
+                            { noremap = true, silent = true, nowait = true } },
+                        position = "center",
+                        align_shortcut = "right",
+                        -- hl = "Statement",
+                        hl_shortcut = "Title",
+                        width = 25,
+                        shrink_margin = true,
+                    }
+                }
+            end
+
+            local buttons = {
+                type = "group",
+                val = {
+                    button("", "Files", "f", "Telescope find_files"),
+                    button("", "Session", "s", "SessionManager load_current_dir_session"),
+                    button("󰠮", "Notes", "n", "Neorg index"),
+                    button("", "Quit", "q", "q!")
                 },
-                footer = {
-                    [[]],
-                    [[]],
-                    [[]],
-                    [[ I am a monument to all your sins. ]],
+            }
+
+            local footer = {
+                type = "text",
+                val = "I am a testament to all your sins.",
+                opts = {
+                    position = "center",
+                    hl = "Constant"
+                }
+            }
+
+            local margin = {
+                type = "padding",
+                val = function()
+                    local height = vim.api.nvim_win_get_height(0)
+                    local occupied = #header.val() + #info.val + #buttons.val + 1 + padding.val * 3
+                    print(occupied)
+                    local value = math.floor((height - occupied) / 2)
+                    return value >= 0 and value or 0
+                end
+            }
+
+
+            alpha.setup({
+                layout = {
+                    margin,
+                    header,
+                    padding,
+                    info,
+                    padding,
+                    buttons,
+                    padding,
+                    footer
                 },
-            },
-        },
-        dependencies = { 'kyazdani42/nvim-web-devicons' },
-    },
+            })
+        end
+    }
 }
