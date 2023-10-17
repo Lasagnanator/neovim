5b5d8e5cdcff69babb76ef113fbf4992b8efae9c
Lasagnanator <d96a07@gmail.com>
2023-10-17 23:55:30 +0200

    Lazy load Vimtex

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -9,6 +9,7 @@ return {
     },
     {
         "lervag/vimtex", -- LaTeX utils
+        ft = { "tex" },
         init = function()
             vim.g.vimtex_compiler_latexmk_engines = {
                 ["_"] = "-lualatex"
