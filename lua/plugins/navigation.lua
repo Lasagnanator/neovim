00b79cecbb923e28e359ea422a52a2258d2ae36d
Lasagnanator <d96a07@gmail.com>
2023-10-23 00:18:05 +0200

    Nvim-tree cleanup

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -6,13 +6,10 @@ return {
             local nvim_tree          = require("nvim-tree")
             local api                = require("nvim-tree.api")
             --<< Settings
-           vim.g.loaded_netrw       = 1
+            vim.g.loaded_netrw       = 1
             vim.g.loaded_netrwPlugin = 1
             --<< Function
             local function on_attach(bufnr)
-                local function opts(desc)
-                    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-                end
                 -- Set defaults
                 api.config.mappings.default_on_attach(bufnr)
 
