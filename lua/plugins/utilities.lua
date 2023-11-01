ca2ca3186ee8d64c3f00451216886afb850bad3c
Lasagnanator <d96a07@gmail.com>
2023-11-02 00:33:04 +0100

    Removed automatic creation and saving of sessions if not loaded

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -11,21 +11,17 @@ return {
         end,
     },
     {
-        "Shatur/neovim-session-manager",                                                                  -- Automatic session manager
+        "Shatur/neovim-session-manager", -- Automatic session manager
         opts = {
-            sessions_dir               = require("plenary.path"):new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
-            path_replacer              = '__',                                                            -- The character to which the path separator will be replaced for session files.
-            colon_replacer             = '++',                                                            -- The character to which the colon symbol will be replaced for session files.
-            autoload_mode              = "Disabled",                                                      -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-            autosave_last_session      = true,                                                            -- Automatically save last session on exit and on session switch.
-            autosave_ignore_not_normal = true,                                                            -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
-            autosave_ignore_dirs       = {},                                                              -- A list of directories where the session will not be autosaved.
-            autosave_ignore_filetypes  = {                                                                -- All buffers of these file types will be closed before the session is saved.
-                'gitcommit',
+            sessions_dir              = require("plenary.path"):new(vim.fn.stdpath('data'), 'sessions'), -- NOTE: not sure if useful
+            autoload_mode             = "Disabled",
+            autosave_ignore_dirs      = {},
+            autosave_ignore_filetypes = {
+                "gitcommit",
+                "gitrebase",
             },
-            autosave_ignore_buftypes   = {},    -- All buffers of these bufer types will be closed before the session is saved.
-            autosave_only_in_session   = false, -- Always autosaves session. If true, only autosaves after a session is active.
-            max_path_length            = 80,    -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
+            autosave_ignore_buftypes  = {},
+            autosave_only_in_session  = true,
         },
         init = function()
             Utils.set_keybinds(Keybinds.session())
