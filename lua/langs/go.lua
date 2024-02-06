a3763dd17bc25efbfa32348e2c17b5aa18922976
Lasagnanator <d96a07@gmail.com>
2024-02-06 21:32:21 +0100

    Added Go plugin

--- lua/langs/go.lua
+++ lua/langs/go.lua
@@ -1,3 +1,19 @@
 if not Langs.go then return {} end
 
-return {}
+return {
+    {
+        'ray-x/go.nvim',
+        dependencies = { -- optional packages
+            'ray-x/guihua.lua',
+            'neovim/nvim-lspconfig',
+            'nvim-treesitter/nvim-treesitter',
+        },
+        -- config = function()
+        --     require('go').setup()
+        -- end,
+        config = true,
+        event = { 'CmdlineEnter' },
+        ft = { 'go', 'gomod' },
+        -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    }
+}
