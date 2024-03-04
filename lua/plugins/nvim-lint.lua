00efa0528b4ef580faf72cc7448db2f1737b96ff
Lasagnanator <d96a07@gmail.com>
2024-03-04 10:31:46 +0100

    Increased aggressiveness of linting

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,7 +1,7 @@
 return {
     "mfussenegger/nvim-lint",
     config = function()
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
             callback = function()
                 require("lint").try_lint()
             end,
