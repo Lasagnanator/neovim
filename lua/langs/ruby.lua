e1a9d3616f19e590f7454a79c63f7746e38ac3f3
Lasagnanator <d96a07@gmail.com>
2024-03-04 10:30:40 +0100

    Update Ruby configuration

--- lua/langs/ruby.lua
+++ lua/langs/ruby.lua
@@ -2,7 +2,8 @@ if not Langs.ruby then return {} end
 
 local utils = require("core.utils")
 
-Mason:update({ "solargraph", "rubocop" })
+-- NOTE: ruby-lsp should be better, but right now is broken (does not install or start, not clear)
+Mason:update({ "solargraph" })
 
 require("lspconfig").solargraph.setup({
     on_attach = utils.on_attach,
@@ -10,9 +11,21 @@ require("lspconfig").solargraph.setup({
     single_file_support = true
 })
 
-require("lint").linters_by_ft.ruby = { "rubocop" }
+-- require("lspconfig").ruby_ls.setup({
+--     on_attach = utils.on_attach,
+--     capabilities = utils.set_capabilities(),
+--     single_file_support = true
+-- })
 
-require("conform").formatters_by_ft.ruby = { "rubocop" }
+-- require("lspconfig").rubocop.setup({
+--     on_attach = utils.on_attach,
+--     capabilities = utils.set_capabilities(),
+--     single_file_support = true
+-- })
+
+-- require("lint").linters_by_ft.ruby = { "rubocop" }
+
+-- require("conform").formatters_by_ft.ruby = { "rubocop" }
 
 return {
     {
