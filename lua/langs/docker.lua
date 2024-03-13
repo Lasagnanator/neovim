39f337f35443484116189b245e050f5f09b746da
Lasagnanator <d96a07@gmail.com>
2024-03-13 18:12:23 +0100

    Added Docker configuration

--- lua/langs/docker.lua
+++ lua/langs/docker.lua
@@ -1,3 +1,22 @@
 if not Langs.docker then return {} end
 
+local utils = require("core.utils")
+
+Treesitter:update("dockerfile")
+Mason:update({ "docker-compose-language-service", "dockerfile-language-server", "hadolint" })
+
+require("lspconfig").dockerls.setup({
+    on_attach = utils.on_attach,
+    capabilities = utils.set_capabilities(),
+    single_file_support = true
+})
+
+require("lspconfig").docker_compose_language_service.setup({
+    on_attach = utils.on_attach,
+    capabilities = utils.set_capabilities(),
+    single_file_support = true
+})
+
+require("lint").linters_by_ft.dockerfile = { "hadolint" }
+
 return {}
