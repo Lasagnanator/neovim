84945acf94d643349bd891c098097c6d2d2e6afb
Lasagnanator <d96a07@gmail.com>
2024-03-27 18:13:13 +0100

    Updated Rust file

--- lua/langs/rust.lua
+++ lua/langs/rust.lua
@@ -1,9 +1,27 @@
 if not Langs.rust then return {} end
 
+local utils = require("core.utils")
+
+Treesitter:update("rust")
+Mason:update({ "rust-analyzer", "codelldb" })
+
 return {
     {
         "mrcjkb/rustaceanvim",
-        ft = "rust"
+        ft = "rust",
+        init = function()
+            vim.g.rustaceanvim = {
+                server = {
+                    -- on_attach = utils.on_attach,
+                    on_attach = function(client, bufnr)
+                        utils.on_attach(client, bufnr)
+                        -- require("lsp-inlayhints").on_attach(client, bufnr)
+                        -- require("lsp-inlayhints").show()
+                    end,
+                    capabilities = utils.set_capabilities(),
+                }
+            }
+        end,
     },
     {
         "saecki/crates.nvim",
