9575d740f36385ba83c7f9ead3abf94723dd53b2
Lasagnanator <d96a07@gmail.com>
2024-03-27 18:12:59 +0100

    Updated Tex file, commented out old location

--- lua/langs/tex.lua
+++ lua/langs/tex.lua
@@ -1,3 +1,20 @@
 if not Langs.tex then return {} end
 
-return {}
+return {
+    {
+        "lervag/vimtex",
+        ft = { "tex" },
+        init = function()
+            vim.g.vimtex_compiler_latexmk_engines = {
+                ["_"] = "-lualatex"
+            }
+            if require("core.utils").is_wsl() then
+                vim.g.vimtex_view_method = 'sioyek'
+                vim.g.vimtex_view_sioyek_exe = 'sioyek.exe'
+                vim.g.vimtex_callback_progpath = 'wsl nvim'
+            else
+                vim.g.vimtex_view_method = 'zathura_simple'
+            end
+        end,
+    },
+}
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,18 +7,18 @@ return {
         end,
         ft = { "markdown" },
     },
-    {
-        "lervag/vimtex", -- LaTeX utils
-        ft = { "tex" },
-        init = function()
-            vim.g.vimtex_compiler_latexmk_engines = {
-                ["_"] = "-lualatex"
-            }
-            vim.g.vimtex_view_method = 'zathura_simple'
-            -- Windows
-            -- vim.g.vimtex_view_method = 'sioyek'
-            -- vim.g.vimtex_view_sioyek_exe = 'sioyek.exe'
-            -- vim.g.vimtex_callback_progpath = 'wsl nvim'
-        end,
-    },
+    -- {
+    --     "lervag/vimtex", -- LaTeX utils
+    --     ft = { "tex" },
+    --     init = function()
+    --         vim.g.vimtex_compiler_latexmk_engines = {
+    --             ["_"] = "-lualatex"
+    --         }
+    --         -- vim.g.vimtex_view_method = 'zathura_simple'
+    --         -- Windows
+    --         vim.g.vimtex_view_method = 'sioyek'
+    --         vim.g.vimtex_view_sioyek_exe = 'sioyek.exe'
+    --         vim.g.vimtex_callback_progpath = 'wsl nvim'
+    --     end,
+    -- },
 }
