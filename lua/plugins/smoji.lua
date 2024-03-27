d45b725da399c37d7f2392d24b5287e7c1e5dc1c
Lasagnanator <d96a07@gmail.com>
2024-03-27 18:11:40 +0100

    Added Smoji

---
+++ lua/plugins/smoji.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "zakissimo/smoji.nvim",
+        config = function()
+            require("smoji")
+            -- TODO: set keymap
+            -- vim.keymap.set("n", "<Leader><Leader>e", "<CMD>Smoji<CR>")
+        end
+    }
+}
