52ff9381f284bad9df3f966c55a4e3a4feddb237
Lasagnanator <d96a07@gmail.com>
2024-03-28 11:43:13 +0100

    Added conditional loading to wslclip

--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ require("core.lazy")
 require("keybinds.base-wk")
 require("core.autocmd")
 
+if require("core.utils").is_wsl() then
+    require("win.wslclip")
+end
+
 if vim.g.neovide then
     require("core.gui")
 end
