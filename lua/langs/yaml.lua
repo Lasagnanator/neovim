fca80f1bdf144b33c219e846ec760becd22e5cbe
Lasagnanator <d96a07@gmail.com>
2024-04-23 15:23:22 +0200

    Added better YAML schema plugin and configuration

--- lua/langs/yaml.lua
+++ lua/langs/yaml.lua
@@ -1,16 +1,43 @@
 if not Langs.yaml then return {} end
 
+local utils = require("core.utils")
+
 Mason:update("yaml-language-server")
 
-require("lspconfig").yamlls.setup({
-    on_attach = Utils.on_attach,
-    capabilities = Utils.set_capabilities(),
-    settings = {
-        yaml = {
-            schemaStore = { enable = false },
-            schemas = require('schemastore').yaml.schemas(),
-        },
-    },
-})
-
-return {}
+return {
+    {
+        "someone-stole-my-name/yaml-companion.nvim",
+        config = function()
+            local config = require("yaml-companion").setup({
+                builtin_matchers = { kubernetes = { enabled = true } },
+                lspconfig = {
+                    on_attach = utils.on_attach,
+                    capabilities = utils.set_capabilities(),
+                    settings = {
+                        redhat = { telemetry = { enabled = false } },
+                        yaml = {
+                            schemaStore = { enable = false },
+                            schemas = require('schemastore').yaml.schemas(),
+                        },
+                    },
+                }
+            })
+            require("lspconfig").yamlls.setup(config)
+            -- TODO: add a keybind for the telescope extension
+            require("telescope").load_extension("yaml_schema")
+            vim.api.nvim_create_user_command("YamlCurrentSchema", function()
+                local schema = require("yaml-companion").get_buf_schema(0)
+                if schema.result[1].name == "none" then
+                    print("Current schema: none")
+                end
+                print("Current schema: " .. schema.result[1].name)
+            end, {})
+        end,
+        ft = { "yaml" },
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        }
+    }
+}
