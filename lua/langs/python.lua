09ad1bbc2bbbeba40d9e9ebab4151f4e04db6243
Lasagnanator <d96a07@gmail.com>
2024-04-26 18:15:57 +0200

    Added Python Treesitter language autoinstall

--- lua/langs/python.lua
+++ lua/langs/python.lua
@@ -4,6 +4,7 @@ local utils = require("core.utils")
 
 -- List from personal laptop
 -- Mason:update({ "autopep8", "debugpy", "mypy", "pydocstyle", "pylint", "pyright" })
+Treesitter:update("python")
 Mason:update({ "debugpy", "pyink", "pylint", "pyright" })
 
 require("lspconfig").pyright.setup({
