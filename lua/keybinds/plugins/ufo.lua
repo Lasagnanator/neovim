c05652a020cc6f82f7af59c6885dc4052d0a03ef
Lasagnanator <d96a07@gmail.com>
2024-05-11 19:16:09 +0200

    Ufo keybinds cleanup

--- lua/keybinds/plugins/ufo.lua
+++ lua/keybinds/plugins/ufo.lua
@@ -3,8 +3,6 @@ local keys   = require("core.classes").Keybinds_group
 local silent = { noremap = true, silent = true }
 
 return keys:new({
-    key:new("n", "<Leader>wd", "<CMD>Alpha<CR>", "Open dashboard", silent),
-
     key:new("n", "zR", function() require('ufo').openAllFolds() end, "Open all folds", silent),
     key:new("n", "zM", function() require('ufo').closeAllFolds() end, "Close all folds", silent),
     -- TODO: evaluate utility, if useful change background colors
