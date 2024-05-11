63d7a2aeefe6f164468c11e29b25073ba78885a6
Lasagnanator <d96a07@gmail.com>
2024-05-11 19:16:44 +0200

    Separated in two sections diffview keybinds

--- lua/keybinds/plugins/diffview.lua
+++ lua/keybinds/plugins/diffview.lua
@@ -1,31 +1,34 @@
+-- WARN: this probably won't work
+local M = {}
+
 local key     = require("core.classes").Keybind
 local keys    = require("core.classes").Keybinds_group
 local silent  = { noremap = true, silent = true }
 local actions = require("diffview.actions")
 
-return keys:new({
-    key:new("n", "<tab>", actions.select_next_entry, "Open the diff for the next file", silent),
-    key:new("n", "<s-tab>", actions.select_prev_entry, "Open the diff for the previous file", silent),
-    key:new("n", "<C-t>", actions.goto_file_tab, "Open the file in a new tabpage", silent),
+M.view = keys:new({
+    key:new("n", "<tab>", actions.select_next_entry, "Open the diff for the next file"),
+    key:new("n", "<s-tab>", actions.select_prev_entry, "Open the diff for the previous file"),
+    key:new("n", "<C-t>", actions.goto_file_tab, "Open the file in a new tabpage"),
     key:new("n", "<Leader>ew", actions.focus_files, "Bring focus to the file panel"),
-    key:new("n", "<Leader>ee", actions.toggle_files, "Toggle the file panel.", silent),
-    -- {
-    --     mode = "n",
-    --     map = "g<C-x>",
-    --     action = actions.cycle_layout,
-    --     opts = { desc = "Cycle through available layouts." }
-    -- },
-    key:new("n", "[m", actions.prev_conflict, "In the merge-tool: jump to the previous conflict", silent),
-    key:new("n", "]m", actions.next_conflict, "In the merge-tool: jump to the next conflict", silent),
-    key:new("n", "<Leader>co", actions.conflict_choose("ours"), "Choose the OURS version of a conflict", silent),
-    key:new("n", "<Leader>ct", actions.conflict_choose("theirs"), "Choose the THEIRS version of a conflict", silent),
-    key:new("n", "<Leader>cb", actions.conflict_choose("base"), "Choose the BASE version of a conflict", silent),
-    key:new("n", "<Leader>ca", actions.conflict_choose("all"), "Choose all the versions of a conflict", silent),
-    key:new("n", "<Leader>q", actions.close, "Close", silent),
-    key:new("n", "dx", actions.conflict_choose("none"), "Delete the conflict region", silent),
-    key:new("n", "<Leader>q", actions.close, "Close", silent),
-    key:new("n", "<Leader>q", actions.close, "Close", silent),
+    key:new("n", "<Leader>ee", actions.toggle_files, "Toggle the file panel."),
+    key:new( "n", "g<C-x>", actions.cycle_layout, "Cycle through available layouts."),
+    key:new("n", "[m", actions.prev_conflict, "In the merge-tool: jump to the previous conflict"),
+    key:new("n", "]m", actions.next_conflict, "In the merge-tool: jump to the next conflict"),
+    key:new("n", "<Leader>co", actions.conflict_choose("ours"), "Choose the OURS version of a conflict"),
+    key:new("n", "<Leader>ct", actions.conflict_choose("theirs"), "Choose the THEIRS version of a conflict"),
+    key:new("n", "<Leader>cb", actions.conflict_choose("base"), "Choose the BASE version of a conflict"),
+    key:new("n", "<Leader>ca", actions.conflict_choose("all"), "Choose all the versions of a conflict"),
+    key:new("n", "<Leader>wq", actions.close, "Close"),
+    key:new("n", "dx", actions.conflict_choose("none"), "Delete the conflict region"),
+    key:new("n", "<Leader>wq", actions.close, "Close"),
+    key:new("n", "<Leader>wq", actions.close, "Close"),
+})
+
+M.global = keys:new({
     key:new('n', '<Leader>gd', '<Cmd>DiffviewOpen<CR>', "Open diffview tab", silent),
     key:new('n', '<Leader>ghd', '<Cmd>DiffviewFileHistory<CR>', "Open commit history", silent),
     key:new('n', '<Leader>ghf', '<Cmd>DiffviewFileHistory %<CR>', "Open this file history", silent),
 })
+
+return M
