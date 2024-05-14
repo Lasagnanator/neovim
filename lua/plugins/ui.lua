b84208861b160dc8a65a789acca99339ca1fab31
Lasagnanator <d96a07@gmail.com>
2024-05-14 11:48:54 +0200

    Added which-key settings

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -301,7 +301,18 @@ return {
         "folke/which-key.nvim",
         config = function()
             local wk = require("which-key")
-            wk.setup()
+            wk.setup({
+                plugins = {
+                    marks = true,
+                    registers = false,
+                    spelling = {
+                        enabled = false
+                    },
+                    presets = {
+                        enabled = false
+                    }
+                },
+            })
             wk.register(require("keybinds.whichkey"))
         end
     }
