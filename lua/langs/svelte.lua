a1f841956a689a5ef9a3c4bfbf5b585f9f8ad11f
Lasagnanator <d96a07@gmail.com>
2024-05-16 17:25:52 +0200

    Updated Svelte files

--- lua/langs/svelte.lua
+++ lua/langs/svelte.lua
@@ -3,14 +3,12 @@ if not Langs.svelte then return {} end
 local utils = require("core.utils")
 
 Treesitter:update("svelte")
-Mason:update("svelte-language-server") -- check if eslint and prettier are useful
+Mason:update({ "svelte-language-server", --[[ "prettierd" ]] }) -- check if eslint and prettier are useful
 
 require("lspconfig").svelte.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
 })
-
--- require("lint").linters_by_ft.svelte = { "eslint_d" }
 
 -- require("conform").formatters_by_ft.eslint = { "prettierd" }
 
