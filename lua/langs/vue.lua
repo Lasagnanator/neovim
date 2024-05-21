4f44bd1740d2af7509f0cba96266e7508b24f7d4
Lasagnanator <d96a07@gmail.com>
2024-05-21 12:38:28 +0200

    Configured Vue tools

--- lua/langs/vue.lua
+++ lua/langs/vue.lua
@@ -1,5 +1,19 @@
 if not Langs.vue then return {} end
 
--- TODO: configure
+local utils = require("core.utils")
+
+Treesitter:update("vue")
+Mason:update("vue-language-server")
+
+require("lspconfig").volar.setup({
+    on_attach = function(_, bufnr)
+        utils.exclude_client("tsserver")
+        utils.on_attach(_, bufnr)
+    end,
+    capabilities = utils.set_capabilities(),
+    root_dir = require("lspconfig").util.root_pattern("**.vue"),
+    single_file_support = false,
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+})
 
 return {}
