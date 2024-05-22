4011e3fc95d0b9849d0e7eadd819b2759888e53e
Lasagnanator <d96a07@gmail.com>
2024-05-22 18:18:56 +0200

    Updated Go config and added inlay hints to LSP

--- lua/langs/go.lua
+++ lua/langs/go.lua
@@ -8,19 +8,35 @@ Mason:update({ "gopls", "delve" })
 require("lspconfig").gopls.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
+    settings = {
+        gopls = {
+            -- TODO: check which options should be disabled
+            hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+            }
+        }
+    }
 })
 
 return {
     {
-        'ray-x/go.nvim',
+        "ray-x/go.nvim",
+        config = function ()
+            require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod", "gosum", "gotmpl", "gowork" },
+        -- build = ":lua require('go.install').update_all_sync()", -- if you need to install/update all binaries
         dependencies = { -- optional packages
-            'ray-x/guihua.lua',
-            'neovim/nvim-lspconfig',
-            'nvim-treesitter/nvim-treesitter',
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
         },
-        config = true,
-        event = { 'CmdlineEnter' },
-        ft = { 'go', 'gomod' },
-        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
     }
 }
