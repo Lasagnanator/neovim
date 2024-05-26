46687c9a3b40c165945913290ae6bf49bc74b4a9
Lasagnanator <d96a07@gmail.com>
2024-05-27 00:47:43 +0200

    Added LSP signature keybinds file

---
+++ lua/keybinds/plugins/signature.lua
@@ -0,0 +1,5 @@
+return {
+    toggle = "<Leader>cu",
+    select_signature = "<A-n>",
+    move_cursor = "<A-m>",
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,9 +6,9 @@ return {
         "ray-x/lsp_signature.nvim", -- Shows signature of function while typing
         opts = {
             doc_lines            = 0,
-            toggle_key           = "<C-0>",
-            select_signature_key = "<A-n>", -- cycle to next signature, e.g. '<M-n>' function overloading
-            move_cursor_key      = "<A-m>", -- imap, use nvim_set_current_win to move cursor between current win and floating
+            toggle_key           = require("keybinds.plugins.signature").toggle,
+            select_signature_key = require("keybinds.plugins.signature").select_signature, -- cycle to next signature, e.g. '<M-n>' function overloading
+            move_cursor_key      = require("keybinds.plugins.signature").move_cursor, -- imap, use nvim_set_current_win to move cursor between current win and floating
             max_height           = 5,       -- max height of signature floating_window
             max_width            = 80,      -- max_width of signature floating_window
             hint_enable          = false,
