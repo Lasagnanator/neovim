1f10cc4ffed264133c8ffcf9f2d580bbd28c69dd
Lasagnanator <d96a07@gmail.com>
2024-05-27 18:35:34 +0200

    Added Telescope explorer and diagnostic keybinds

--- lua/keybinds/plugins/telescope.lua
+++ lua/keybinds/plugins/telescope.lua
@@ -3,10 +3,11 @@ local keys   = require("core.classes").Keybinds_group
 local silent = { noremap = true, silent = true }
 
 return keys:new({
+    key:new("n", "<Leader>fb", require("telescope").extensions.file_browser.file_browser, "Browser", silent),
+    key:new("n", "<Leader>fe", require("telescope.builtin").diagnostics, "Browser", silent),
     key:new("n", "<Leader>ff", require("telescope.builtin").find_files, "Files", silent),
+    key:new("n", "<Leader>fh", require("telescope.builtin").help_tags, "Help", silent),
+    key:new("n", "<Leader>fk", require("telescope.builtin").buffers, "Buffers", silent),
     -- key:new("n", "<Leader>fw", require("telescope.builtin").live_grep, "Live grep", silent),
     key:new("n", "<Leader>fw", require("telescope").extensions.live_grep_args.live_grep_args, "Live grep", silent),
-    key:new("n", "<Leader>fk", require("telescope.builtin").buffers, "Buffers", silent),
-    key:new("n", "<Leader>fh", require("telescope.builtin").help_tags, "Help", silent),
-    key:new("n", "<Leader>fe", require("telescope").extensions.file_browser.file_browser, "Explorer", silent),
 })
