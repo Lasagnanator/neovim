6d260757819f0b2a99bdd6444a1aada22cc7d863
Lasagnanator <d96a07@gmail.com>
2024-06-02 20:17:14 +0200

    Added more Noice keybindings

--- lua/keybinds/plugins/noice.lua
+++ lua/keybinds/plugins/noice.lua
@@ -3,5 +3,8 @@ local keys = require("core.classes").Keybinds_group
 local silent   = { noremap = true, silent = true }
 
 return keys:new({
-    key:new("n", "<Leader>ud", "<Cmd>NoiceDismiss<CR>", "Dismiss Noice Warning", silent)
+    key:new("n", "<Leader>ud", "<Cmd>NoiceDismiss<CR>", "Dismiss notifications", silent),
+    key:new("n", "<Leader>ua", "<Cmd>NoiceAll<CR>", "Show all messages", silent),
+    key:new("n", "<Leader>ue", "<Cmd>NoiceErrors<CR>", "Show error messages", silent),
+    key:new("n", "<Leader>fu", "<Cmd>NoiceTelescope<CR>", "Notification", silent),
 })
