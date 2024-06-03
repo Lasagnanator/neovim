b728fd5081646d37424aaa70ab9fa771ac20591e
Lasagnanator <d96a07@gmail.com>
2024-06-03 17:23:47 +0200

    Configured base Zig language

--- lua/langs/zig.lua
+++ lua/langs/zig.lua
@@ -1,5 +1,13 @@
 if not Langs.zig then return {} end
 
--- TODO: configure
+local utils = require("core.utils")
+
+Treesitter:update("zig")
+Mason:update({ "zls", "codelldb" })
+
+require("lspconfig").zls.setup({
+    on_attach = utils.on_attach,
+    capabilities = utils.set_capabilities(),
+})
 
 return {}
