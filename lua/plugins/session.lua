218a5fc945c874b138cc191c7ff6094858a1ee90
Lasagnanator <d96a07@gmail.com>
2024-06-07 22:54:27 +0200

    Fixed session manager's config

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,16 +1,18 @@
 return {
-    "Shatur/neovim-session-manager",                                                                     -- Automatic session manager
-    opts = {
-        sessions_dir              = require("plenary.path"):new(vim.fn.stdpath('data'), 'sessions'),     -- NOTE: not sure if useful
-        autoload_mode             = "Disabled",
-        autosave_ignore_dirs      = {},
-        autosave_ignore_filetypes = {
-            "gitcommit",
-            "gitrebase",
-        },
-        autosave_ignore_buftypes  = {},
-        autosave_only_in_session  = true,
-    },
-    init = function() require("keybinds.plugins.session"):set() end,
+    "Shatur/neovim-session-manager", -- Automatic session manager
+    config = function()
+        require("session_manager").setup({
+            sessions_dir = require("plenary.path"):new(vim.fn.stdpath('data'), 'sessions'), -- NOTE: not sure if useful
+            autoload_mode = require("session_manager.config").AutoloadMode.Disabled,
+            autosave_ignore_dirs = {},
+            autosave_ignore_filetypes = {
+                "gitcommit",
+                "gitrebase",
+            },
+            autosave_ignore_buftypes  = {},
+            autosave_only_in_session  = true,
+        })
+        require("keybinds.plugins.session"):set()
+    end,
     dependencies = { "nvim-lua/plenary.nvim" },
 }
