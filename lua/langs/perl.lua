9634da000451b9d3263f6bcdf4f15ede04668467
Lasagnanator <d96a07@gmail.com>
2024-06-10 00:47:46 +0200

    Changed perlnavigator with perlpls

--- lua/langs/perl.lua
+++ lua/langs/perl.lua
@@ -3,12 +3,11 @@ if not Langs.perl then return {} end
 local utils = require("core.utils")
 
 Treesitter:update("perl")
-Mason:update({ "perlnavigator", "perl-debug-adapter" })
+Mason:update("perl-debug-adapter")
 
-require("lspconfig").perlnavigator.setup({
+require("lspconfig").perlpls.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
-    single_file_support = true,
 })
 
 return {}
