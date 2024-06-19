a387cce8c24bb17498a6636b7033cc1e33d6369c
Lasagnanator <d96a07@gmail.com>
2024-06-19 18:21:24 +0200

    Fixed Terraform config

--- lua/langs/terraform.lua
+++ lua/langs/terraform.lua
@@ -5,9 +5,10 @@ local utils = require("core.utils")
 Treesitter:update("terraform")
 Mason:update({ "terraform-ls", "tflint" })
 
-require("lspconfig").terraform_lsp.setup({
+require("lspconfig").terraformls.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
+    single_file_support = true,
 })
 
 require("lint").linters_by_ft.terraform = { "tflint" }
