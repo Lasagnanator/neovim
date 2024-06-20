71a59f7e93834af1700a8eb41be7f06c74cce576
Lasagnanator <d96a07@gmail.com>
2024-06-20 16:22:34 +0200

    Removed single file support for Terraform and added tab settings

--- lua/langs/terraform.lua
+++ lua/langs/terraform.lua
@@ -8,7 +8,6 @@ Mason:update({ "terraform-ls", "tflint" })
 require("lspconfig").terraformls.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
-    single_file_support = true,
 })
 
 require("lint").linters_by_ft.terraform = { "tflint" }
