0b3d7ea7b57a71e1e8d10ec65309d089f27a7570
Lasagnanator <d96a07@gmail.com>
2024-06-22 15:55:37 +0200

    Changed LSP hover keymap

--- lua/keybinds/plugins/lsp.lua
+++ lua/keybinds/plugins/lsp.lua
@@ -8,7 +8,7 @@ function M.on_attach(args)
         key:new("n", "<Leader>cr", vim.lsp.buf.rename, "Rename symbol", args),
         key:new("n", "<Leader>cd", function() vim.lsp.buf.definition({ reuse_win = true, }) end, "Goto definition", args),
         key:new("n", "<Leader>cD", function() vim.lsp.buf.declaration({ reuse_win = true, }) end, "Goto declaration", args),
-        key:new("n", "<Leader>ci", vim.lsp.buf.hover, "Open hover", args),
+        key:new("n", "<C-h>", vim.lsp.buf.hover, "Open hover", args),
         key:new("n", "<Leader>ca", vim.lsp.buf.code_action, "Code actions", args),
         key:new("n", "<Leader>cwa", vim.lsp.buf.add_workspace_folder, "Add workspace folder", args),
         key:new("n", "<Leader>cwr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder", args),
