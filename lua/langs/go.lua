1fed8f6a81bac8dd0209f1e60fbd392b67ae4db1
Lasagnanator <d96a07@gmail.com>
2024-06-25 00:01:07 +0200

    Added lots of settings for Gopls

--- lua/langs/go.lua
+++ lua/langs/go.lua
@@ -3,13 +3,50 @@ if not Langs.go then return {} end
 local utils = require("core.utils")
 
 Treesitter:update({ "go", "gomod", "gosum", "gotmpl", "gowork" })
-Mason:update({ "gopls", "delve" })
+Mason:update({ "gopls", "delve", "gofumpt", "gomodifytags", "impl" })
 
 require("lspconfig").gopls.setup({
-    on_attach = utils.on_attach,
+    -- workaround for gopls not supporting semanticTokensProvider
+    on_attach = function(client, bufnr)
+        utils.on_attach(client, bufnr)
+        if not client.server_capabilities.semanticTokensProvider then
+            local semantic = client.config.capabilities.textDocument.semanticTokens
+            client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                    tokenTypes = semantic.tokenTypes,
+                    tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+            }
+        end
+    end,
     capabilities = utils.set_capabilities(),
     settings = {
         gopls = {
+            gofumpt = true,
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            semanticTokens = true,
+            analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+            },
+            codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+            },
             -- TODO: check which options should be disabled
             hints = {
                 assignVariableTypes = true,
@@ -19,7 +56,7 @@ require("lspconfig").gopls.setup({
                 functionTypeParameters = true,
                 parameterNames = true,
                 rangeVariableTypes = true,
-            }
+            },
         }
     }
 })
@@ -27,7 +64,7 @@ require("lspconfig").gopls.setup({
 return {
     {
         "ray-x/go.nvim",
-        config = function ()
+        config = function()
             require("go").setup()
         end,
         event = { "CmdlineEnter" },
@@ -41,7 +78,7 @@ return {
     },
     {
         "leoluz/nvim-dap-go",
-        config = function (_, opts)
+        config = function(_, opts)
             require("dap-go").setup(opts)
         end,
         dependencies = {
