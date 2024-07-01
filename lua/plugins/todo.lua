fb60b7d8b06bbd98508d9d08acbd8020ce8658f1
Lasagnanator <d96a07@gmail.com>
2024-07-01 17:57:03 +0200

    Changed To-Do comments style

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -3,6 +3,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         highlight = {
+            keyword = "bg",
             pattern = [[.*<(KEYWORDS)]],
         }
     },
