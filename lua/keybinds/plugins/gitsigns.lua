8cb88f52e01f07c8c90d3f741138198765d60509
Lasagnanator <d96a07@gmail.com>
2024-07-08 12:31:14 +0200

    Changed next/prev hunk shortcut

--- lua/keybinds/plugins/gitsigns.lua
+++ lua/keybinds/plugins/gitsigns.lua
@@ -7,6 +7,6 @@ return keys:new({
     key:new("n", "<Leader>gss", "<CMD>Gitsigns toggle_signs<CR>",               "Toggle Git signs", silent),
     key:new("n", "<Leader>gsn", "<CMD>Gitsigns toggle_numhl<CR>",               "Toggle number highlight", silent),
     key:new("n", "<Leader>gsl", "<CMD>Gitsigns toggle_linehl<CR>",              "Toggle linehighlight", silent),
-    key:new("n", "]h",          function() require("gitsigns").next_hunk() end, "Hunk", silent),
-    key:new("n", "[h",          function() require("gitsigns").prev_hunk() end, "Hunk", silent),
+    key:new("n", "]c",          function() require("gitsigns").next_hunk() end, "Hunk", silent),
+    key:new("n", "[c",          function() require("gitsigns").prev_hunk() end, "Hunk", silent),
 })
