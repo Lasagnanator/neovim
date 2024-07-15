25593dd8a27ec3654af84166a197b3e12b842829
Lasagnanator <d96a07@gmail.com>
2024-07-15 21:31:52 +0200

    Open Neorg windows in new tab

--- lua/keybinds/plugins/neorg.lua
+++ lua/keybinds/plugins/neorg.lua
@@ -3,9 +3,9 @@ local keys = require("core.classes").Keybinds_group
 local silent   = { noremap = true, silent = true }
 
 return keys:new({
-    key:new("n", "<Leader>ni", "<Cmd>Neorg index<CR>", "Index", silent),
+    key:new("n", "<Leader>ni", "<Cmd>tabnew<CR><Cmd>Neorg index<CR>", "Index", silent),
     key:new("n", "<Leader>nq", "<Cmd>Neorg return<CR>", "Close", silent),
-    key:new("n", "<Leader>nj", "<Cmd>Neorg journal<CR>", "Journal", silent),
+    key:new("n", "<Leader>nj", "<Cmd>tabnew<CR><Cmd>Neorg journal<CR>", "Journal", silent),
     key:new("n", "<Leader>nt", "<Cmd>Neorg toc<CR>", "Table of contents", silent),
     key:new("n", "<Leader>ngm", "<Cmd>Neorg inject-metadata<CR>", "Inject metadata", silent),
     key:new("n", "<Leader>ngw", "<Cmd>Neorg generate-workspace-summary<CR>", "Generate workspace summary", silent),
