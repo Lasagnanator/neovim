ffdbc2a24c961430511abf9410ef199f2cb140c2
Lasagnanator <d96a07@gmail.com>
2024-07-17 01:11:30 +0200

    Added Marksman and Vale to markdown config

--- lua/langs/markdown.lua
+++ lua/langs/markdown.lua
@@ -1,6 +1,20 @@
 if not Langs.markdown then return {} end
 
+local utils = require("core.utils")
+
 Treesitter:update({ "markdown", "markdown_inline" })
+Mason:update({ "marksman", "vale" })
+
+require("lspconfig").marksman.setup({
+    on_attach = utils.on_attach,
+    capabilities = utils.set_capabilities(),
+})
+
+local lint = require("lint")
+lint.linters_by_ft.markdown = { "vale" }
+local vale_opts = lint.linters.vale.args
+table.insert(vale_opts, "--config=" .. vim.fn.stdpath('config') .. "/files/vale.ini")
+lint.linters.vale.args = vale_opts
 
 return {
     {
