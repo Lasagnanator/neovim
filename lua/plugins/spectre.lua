384f630d2dc7e3b2d5aa2fd4ee230e29fe40f4f5
Lasagnanator <d96a07@gmail.com>
2024-07-26 17:58:54 +0200

    Added TODO for nvim-spectre

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-pack/nvim-spectre",
     config = true,
+     -- TODO: Add keybinds under Leader-m
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons"
