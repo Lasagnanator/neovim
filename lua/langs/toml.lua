313cac05636c62a8fa24f379b9b8f8eee24246d0
Lasagnanator <d96a07@gmail.com>
2024-07-26 22:41:21 +0200

    Added taplo setup

--- lua/langs/toml.lua
+++ lua/langs/toml.lua
@@ -5,4 +5,9 @@ local utils = require("core.utils")
 Treesitter:update("toml")
 Mason:update("taplo")
 
+require("lspconfig").taplo.setup({
+    on_attach = utils.on_attach,
+    capabilities = utils.set_capabilities(),
+})
+
 return {}
