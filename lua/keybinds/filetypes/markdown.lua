9ccf58cc4ccf74ae5595b9239618890901af1fbc
Lasagnanator <d96a07@gmail.com>
2024-07-31 18:03:15 +0200

    Added markdown rendering toggle and aligned keybinds description

--- lua/keybinds/filetypes/markdown.lua
+++ lua/keybinds/filetypes/markdown.lua
@@ -3,5 +3,6 @@ local keys = require("core.classes").Keybinds_group
 local silent = { noremap = true, silent = true }
 
 return keys:new({
-    key:new( "", "<Leader>mp", "<Cmd>MarkdownPreviewToggle<CR>",  "Toggle markdown preview", silent ),
+    key:new( "", "<Leader>mp", "<Cmd>MarkdownPreviewToggle<CR>",  "Toggle preview", silent ),
+    key:new( "", "<Leader>mr", "<Cmd>RenderMarkdown toggle<CR>",  "Toggle rendering", silent ),
 })
