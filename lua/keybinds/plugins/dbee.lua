b6af38ac7697b753af51c6a3ff6d4e5553d8ac0a
Lasagnanator <d96a07@gmail.com>
2024-08-06 00:59:37 +0200

    Changed Vim-dadbod with nvim-dbee

---
+++ lua/keybinds/plugins/dbee.lua
@@ -0,0 +1,7 @@
+local key = require("core.classes").Keybind
+local keys = require("core.classes").Keybinds_group
+local silent   = { noremap = true, silent = true }
+
+return keys:new({
+    key:new("n", "<Leader>oq", function() require('dbee').open() end, "Open database client", silent)
+})
--- lua/langs/sql.lua
+++ lua/langs/sql.lua
@@ -34,16 +34,19 @@ require("conform").formatters_by_ft.sql = { "sql_formatter" }
 
 return {
     {
-        "tpope/vim-dadbod", -- Database engine
-        ft = { "sql", "mysql", "psql" },
-        cmd = { "DBUI", "DBUIToggle" },
-        config = function()
-            vim.g.db_ui_use_nerd_fonts = 1
-            vim.g.db_ui_win_position = "right"
+        "kndndrj/nvim-dbee",
+        config = function ()
+            require("dbee").setup()
+        end,
+        keys = require("keybinds.plugins.dbee"):to_lazy(),
+        build = function()
+            -- Install tries to automatically detect the install method.
+            -- if it fails, try calling it with one of these parameters:
+            --    "curl", "wget", "bitsadmin", "go"
+            require("dbee").install()
         end,
         dependencies = {
-            "kristijanhusak/vim-dadbod-ui",     -- UI for interacting with databases
-            "kristijanhusak/vim-dadbod-completion", -- Completion (needed outside of LSP?)
+            "MunifTanjim/nui.nvim",
         },
-    }
+    },
 }
