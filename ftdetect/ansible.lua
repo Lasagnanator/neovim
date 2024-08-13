1564bf49b17dd9c7397de71253fcd9e4ffadde3f
Lasagnanator <d96a07@gmail.com>
2024-08-13 18:04:16 +0200

    Improved Ansible files detection

--- ftdetect/ansible.lua
+++ ftdetect/ansible.lua
@@ -1,19 +1,21 @@
 local function is_ansible(_, bufnr)
-    local content = vim.api.nvim_buf_get_lines(bufnr, 0, 2, false) or { "" }
-    local ansible = false
+    local content = vim.api.nvim_buf_get_lines(bufnr, 0, 10, false) or { "" }
+    local matched_lines = 0
     for _, line in pairs(content) do
         if
-            vim.regex([[.*- name:]]):match_str(line) ~= nil or
+            vim.regex([[.*name:]]):match_str(line) ~= nil or
             vim.regex([[.*hosts:]]):match_str(line) ~= nil or
             vim.regex([[.*become:]]):match_str(line) ~= nil or
             vim.regex([[.*tasks:]]):match_str(line) ~= nil or
+            vim.regex([[.*collections:]]):match_str(line) ~= nil or
             vim.regex([[.*vars_files:]]):match_str(line) ~= nil
         then
-            ansible = true
-            return 'yaml.ansible'
+            matched_lines = matched_lines + 1
         end
     end
-    if not ansible then
+    if matched_lines >= 3 then
+        return 'yaml.ansible'
+    else
         return 'yaml'
     end
 end
@@ -30,6 +32,7 @@ vim.filetype.add({
         [".*/roles/.*/handlers/.*%.yml"] = "yaml.ansible",
         [".*/roles/.*/handlers/.*%.yaml"] = "yaml.ansible",
         [".*%.yml"] = is_ansible,
-        [".*%.yaml"] = is_ansible
+        [".*%.yaml"] = is_ansible,
+        ["inventory_?.*"] = "confini"
     }
 })
