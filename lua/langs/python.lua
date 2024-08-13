de52e26f3f79a73282237b0c30302e9860d69397
Lasagnanator <d96a07@gmail.com>
2024-08-13 18:05:22 +0200

    Changed Python linter and LSP configurations
    Added options to pylint, so it can work with virtualenvs
    Changed basedpyright with pyright until I figure out the configuration

--- lua/langs/python.lua
+++ lua/langs/python.lua
@@ -3,9 +3,10 @@ if not Langs.python then return {} end
 local utils = require("core.utils")
 
 Treesitter:update("python")
-Mason:update({ "debugpy", "pyink", "pylint", "basedpyright" })
+-- Mason:update({ "debugpy", "pyink", "pylint", "basedpyright" })
+Mason:update({ "debugpy", "pyink", "pylint", "pyright" })
 
-require("lspconfig").basedpyright.setup({
+require("lspconfig").pyright.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
     settings = {
@@ -21,6 +22,7 @@ require("lspconfig").basedpyright.setup({
 })
 
 require('lint').linters_by_ft.python = { "pylint" }
+require("lint").linters.pylint.args = { "-m", "pylint", "-f", "json", "--disable=import-error,too-few-public-methods,unused-import,unused-argument" }
 
 require("conform").formatters_by_ft.python = { "pyink" }
 
