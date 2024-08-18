fd8c17d6519878ae6ed808c5eb228887945f4d02
Lasagnanator <d96a07@gmail.com>
2024-08-18 22:54:15 +0200

    Removed Transfer

--- lua/plugins/transfer.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "coffebar/transfer.nvim",
-    config = true,
-    cmd = { "TransferInit", "DiffRemote", "TransferUpload", "TransferDownload", "TransferDirDiff", "TransferRepeat" },
-    keys = require("keybinds.plugins.transfer"):to_lazy()
-}
