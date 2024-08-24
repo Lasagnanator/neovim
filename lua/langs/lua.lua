e7ef9a9dbcf17bb0805a36a0ce9dafd3cac9e5c9
Lasagnanator <d96a07@gmail.com>
2024-08-24 19:51:41 +0200

    Removed unusued Lua settings

--- lua/langs/lua.lua
+++ lua/langs/lua.lua
@@ -5,19 +5,9 @@ local utils = require("core.utils")
 Treesitter:update("lua")
 Mason:update("lua-language-server")
 
--- if Langs.nvim then require("neodev").setup() end
-
 require("lspconfig").lua_ls.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities(),
-    on_new_config = function(client)
-        -- if vim.fn.expand('%:p'):match("/awesome/") then
-        if vim.api.nvim_buf_get_name(0):match("/awesome/") then
-            for _, val in ipairs({ "awesome", "client", "screen" }) do
-                table.insert(client.settings.Lua.diagnostics.globals, val)
-            end
-        end
-    end,
     settings = {
         Lua = {
             telemetry = {
