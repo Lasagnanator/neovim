d62c423bdf2fc3a89ef8ccae80144969c28ad998
Lasagnanator <d96a07@gmail.com>
2024-08-27 20:00:57 +0200

    Changed Dbee bind

--- lua/keybinds/plugins/dbee.lua
+++ lua/keybinds/plugins/dbee.lua
@@ -3,5 +3,5 @@ local keys = require("core.classes").Keybinds_group
 local silent   = { noremap = true, silent = true }
 
 return keys:new({
-    key:new("n", "<Leader>oq", function() require('dbee').open() end, "Open database client", silent)
+    key:new("n", "<Leader>ob", function() require('dbee').open() end, "Open database client", silent)
 })
