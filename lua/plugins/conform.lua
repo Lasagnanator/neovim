053c41eae5d3030ebb5a5f5e68c13cb5f5ba5a64
Lasagnanator <d96a07@gmail.com>
2024-09-02 00:30:44 +0200

    Added LSP fallback to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,5 +1,10 @@
 return {
     {
-        "stevearc/conform.nvim"
+        "stevearc/conform.nvim",
+        opts = {
+            default_format_opts = {
+                lsp_format = "fallback",
+            },
+        }
     }
 }
