8aca0e0815667e43f1a5083cd1024eb103d812ee
Lasagnanator <d96a07@gmail.com>
2024-09-10 12:51:51 +0200

    Added markview and disabled markdown-preview

--- lua/langs/markdown.lua
+++ lua/langs/markdown.lua
@@ -29,10 +29,32 @@ return {
     },
     -- TODO: try the other markdown candy plugin
     {
+        enabled = false,
         "MeanderingProgrammer/markdown.nvim",
         main = "render-markdown",
         opts = {},
         ft = { "markdown" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        }
+    },
+    {
+        "OXY2DEV/markview.nvim",
+        enabled = true,
+        opts = {
+            modes = { "n", "i", "no", "c" },
+            hybrid_modes = { "i" },
+
+            -- This is nice to have
+            callbacks = {
+                on_enable = function(_, win)
+                    vim.wo[win].conceallevel = 2;
+                    vim.wo[win].concealcursor = "nc";
+                end
+            }
+        },
+        ft = "markdown",
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
             "nvim-tree/nvim-web-devicons"
