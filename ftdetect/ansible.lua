02fcfe033fadff5f34385a863e6e34b65271d28d
Lasagnanator <d96a07@gmail.com>
2024-09-20 10:15:35 +0200

    Moved generic Ansible detection up because it seems to work better with path detection

--- ftdetect/ansible.lua
+++ ftdetect/ansible.lua
@@ -32,14 +32,14 @@ vim.filetype.add({
         inventory = "confini"
     },
     pattern = {
+        [".*%.yml"] = is_ansible,
+        [".*%.yaml"] = is_ansible,
         [".*/playbooks/.*%.yml"] = "yaml.ansible",
         [".*/playbooks/.*%.yaml"] = "yaml.ansible",
         [".*/roles/.*/tasks/.*%.yml"] = "yaml.ansible",
         [".*/roles/.*/tasks/.*%.yaml"] = "yaml.ansible",
         [".*/roles/.*/handlers/.*%.yml"] = "yaml.ansible",
         [".*/roles/.*/handlers/.*%.yaml"] = "yaml.ansible",
-        [".*%.yml"] = is_ansible,
-        [".*%.yaml"] = is_ansible,
         ["inventory_.*"] = "confini"
     }
 })
