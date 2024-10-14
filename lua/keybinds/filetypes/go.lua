f47541b828dc749bcca48e6a42d305e8d67ec9ea
Lasagnanator <d96a07@gmail.com>
2024-10-14 18:44:22 +0200

    Added go get keybind

--- lua/keybinds/filetypes/go.lua
+++ lua/keybinds/filetypes/go.lua
@@ -5,6 +5,7 @@ local silent = { noremap = true, silent = true }
 return keys:new({
     key:new("", "<Leader>mb", "<Cmd>GoBuild<CR>", "Build", silent),
     key:new("", "<Leader>mi", "<Cmd>GoImports<CR>", "Format imports", silent),
+    key:new("", "<Leader>mi", "<Cmd>GoGet<CR>", "Get missing packages", silent),
     key:new("", "<Leader>mr", "<Cmd>GoRun<CR>", "Run", silent),
     key:new("", "<Leader>mt", "<Cmd>GoModTidy<CR>", "Tidy", silent),
 })
