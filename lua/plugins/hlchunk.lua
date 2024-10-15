3d2b0421586ec3f7840f50f09cde0e391392e2f7
Lasagnanator <d96a07@gmail.com>
2024-10-16 01:05:03 +0200

    Added recursive search for hlchunk color

--- lua/plugins/hlchunk.lua
+++ lua/plugins/hlchunk.lua
@@ -1,11 +1,24 @@
 return {
     "shellRaining/hlchunk.nvim",
     config = function()
+
+        local function get_color(hl)
+            local hl_link = vim.api.nvim_get_hl(0, { name = hl }).link
+            if hl_link ~= nil then
+                return get_color(hl_link)
+            end
+            local color = vim.api.nvim_get_hl(0, { name = hl }).fg or
+                vim.api.nvim_get_hl(0, { name = hl }).guifg or
+                vim.api.nvim_get_hl(0, { name = hl }).ctermfg
+            return color
+        end
+
         local colors = {
-            chunk = string.format("#%x", vim.api.nvim_get_hl(0, { name = "CursorLineNr" }).fg),
-            error = string.format("#%x", vim.api.nvim_get_hl(0, { name = "ErrorMsg" }).fg),
-            indent = string.format("#%x", vim.api.nvim_get_hl(0, { name = "LineNr" }).fg) -- NOTE: consider hlgroup Whitespace
+            chunk = string.format("#%x", get_color("CursorLineNr")),
+            error = string.format("#%x", get_color("ErrorMsg")),
+            indent = string.format("#%x", get_color("LineNr")) -- NOTE: consider hlgroup Whitespace
         }
+
         require("hlchunk").setup({
             chunk = {
                 enable = true,
