d178fd153b523faa505bb9fc6a2a1356c972c3f1
Lasagnanator <d96a07@gmail.com>
2024-10-19 18:28:29 +0200

    Increased Treesj max lenght

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,6 +2,7 @@ return {
     "Wansmer/treesj",
     opts = {
         use_default_keymaps = false,
+        max_join_length = 200,
     },
     lazy = false,
     keys = require("keybinds.plugins.treesj"):to_lazy(),
