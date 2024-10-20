3a10908e0392149ae2a35e62e930ee4691de7de7
Lasagnanator <d96a07@gmail.com>
2024-10-21 00:39:19 +0200

    Added gitgraph plugin

---
+++ lua/plugins/gitgraph.lua
@@ -0,0 +1,50 @@
+local symbols = {
+    merge_commit = '',
+    commit = '',
+    merge_commit_end = '',
+    commit_end = '',
+
+    -- Advanced symbols
+    GVER = '',
+    GHOR = '',
+    GCLD = '',
+    GCRD = '╭',
+    GCLU = '',
+    GCRU = '',
+    GLRU = '',
+    GLRD = '',
+    GLUD = '',
+    GRUD = '',
+    GFORKU = '',
+    GFORKD = '',
+    GRUDCD = '',
+    GRUDCU = '',
+    GLUDCD = '',
+    GLUDCU = '',
+    GLRDCL = '',
+    GLRDCR = '',
+    GLRUCL = '',
+    GLRUCR = '',
+}
+
+return {
+    "isakbm/gitgraph.nvim",
+    lazy = false,
+    opts = {
+        -- symbols = symbols,
+        hooks = {
+            -- Check diff of a commit
+            on_select_commit = function(commit)
+                vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
+                vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
+            end,
+            -- Check diff from commit a -> commit b
+            on_select_range_commit = function(from, to)
+                vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+                vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+            end,
+        },
+    },
+    keys = require("keybinds.plugins.gitgraph"):to_lazy(),
+    dependencies = { 'sindrets/diffview.nvim' }
+}
