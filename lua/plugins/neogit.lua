ab1d784d29dd6eedcedef45a903759d80fbf1cb6
Lasagnanator <d96a07@gmail.com>
2024-10-21 00:41:20 +0200

    Changed Neogit's settings

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,9 +1,15 @@
 return {
     "NeogitOrg/neogit",
-    config = true,
-    init = function()
-        require("keybinds.plugins.neogit"):set()
-    end,
+    lazy = false,
+    opts = {
+        disable_hint = true,
+        graph_style = "unicode",
+        console_timeout = 0,
+        commit_editor = {
+            spell_check = false,
+        },
+    },
+    keys = require("keybinds.plugins.neogit"):to_lazy(),
     dependencies = {
         "nvim-lua/plenary.nvim",
         "ibhagwan/fzf-lua",
