3570c5777f975e7359c535a8faf11b7c5f5fd63b
Lasagnanator <d96a07@gmail.com>
2024-10-28 15:12:30 +0100

    Added typewriter mode keybind

--- lua/plugins/typewriter.lua
+++ lua/plugins/typewriter.lua
@@ -3,5 +3,7 @@ return {
     opts = {
         enable_horizontal_scroll = false,
     },
+    keys = require("keybinds.plugins.typewriter"):to_lazy(),
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-treesitter/nvim-treesitter" }
 }
