56258e784696881c6c76b674d19953029fa5c443
Lasagnanator <d96a07@gmail.com>
2024-10-31 00:28:34 +0100

    Removed filetypes folder and files

--- after/ftplugin/jsonc.lua
+++ after/ftplugin/jsonc.lua
@@ -1,4 +1,6 @@
 --<< JSONC FILETYPE CONFIG >>--
 
 --<< Settings
-require("filetypes.web")
+vim.opt_local["shiftwidth"] = 2
+vim.opt_local["tabstop"] = 2
+vim.opt_local["softtabstop"] = 2
--- lua/filetypes/web.lua
+++
@@ -1,6 +0,0 @@
---<< WEB DEVELOPMENT CONFIG >>--
-
---<< Settings
-vim.opt_local["shiftwidth"] = 2
-vim.opt_local["tabstop"] = 2
-vim.opt_local["softtabstop"] = 2
