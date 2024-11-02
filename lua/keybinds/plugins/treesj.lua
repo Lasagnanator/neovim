e1493d4985772e44cfa6ddcf83035aeedf672d04
Lasagnanator <d96a07@gmail.com>
2024-11-02 19:26:52 +0100

    Moved treesj to text manipulation

--- lua/keybinds/plugins/treesj.lua
+++ lua/keybinds/plugins/treesj.lua
@@ -3,6 +3,5 @@ local keys   = require("core.classes").Keybinds_group
 local silent = { noremap = true, silent = true }
 
 return keys:new({
-    -- TODO: change with <leader>l for all text manipulation
-    key:new("n", "<C-j>", function() require("treesj").toggle() end, "Open dashboard", silent)
+    key:new("n", "<Leader>aj", function() require("treesj").toggle() end, "Open dashboard", silent)
 })
