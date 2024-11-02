62faf68bbfa45596ec7ec8dafc0a367eb6956f25
Lasagnanator <d96a07@gmail.com>
2024-11-02 19:25:40 +0100

    Reworked clangs configuration

--- lua/langs/clang.lua
+++ lua/langs/clang.lua
@@ -3,16 +3,37 @@ if not Langs.clang then return {} end
 local utils = require("core.utils")
 
 Treesitter:update({ "c", "cpp" })
-Mason:update({ "clangd", "codelldb", "cpplint" })
+Mason:update({ "clangd", "codelldb", "cpplint", "clang-format" })
 
 require("lspconfig").clangd.setup({
     on_attach = utils.on_attach,
     capabilities = utils.set_capabilities({
         offsetEncoding = "UTF-16",
     }),
+    settings = {
+
+    }
 })
 
--- TODO: setup codelldb and cpplint
--- cpplint:         args = { "--filter=-legal/copyright", "$FILENAME" }
--- clang_format:    extra_args = { "--style", "file:" .. os.getenv("HOME") .. "/.config/nvim/files/clang-format" } }
+require("lint").linters.cpplint.args = { "--filter=-legal/copyright", "$FILENAME" }
+require("lint").linters_by_ft.c = { "cpplint" }
+require("lint").linters_by_ft.cpp = { "cpplint" }
+
+require("conform").formatters.clang_format = {
+    command = "clang-format",
+    append_args = {
+        "--style",
+        "file:" .. os.getenv("HOME") .. "/.config/nvim/files/clang-format",
+    }
+}
+require("conform").formatters["clang-format"] = {
+    append_args = {
+        "--style",
+        "file:" .. os.getenv("HOME") .. "/.config/nvim/files/clang-format",
+    }
+}
+require("conform").formatters_by_ft.c = { "clang-format" }
+require("conform").formatters_by_ft.cpp = { "clang-format" }
+
+-- TODO: setup codelldb
 return {}
