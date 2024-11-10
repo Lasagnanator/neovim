4f83fd4fa958726b647fce84cd459d8f87f2613b
Lasagnanator <d96a07@gmail.com>
2024-11-11 00:25:47 +0100

    Reworked pairs

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -5,41 +5,16 @@ return {
             enable_check_bracket_line = false,
             ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
         },
-        init = function()
-            local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
-            local autopairs = require("nvim-autopairs")
-            local rule = require("nvim-autopairs.rule")
-            autopairs.add_rules {
-                rule(' ', ' ')
-                    :with_pair(function(opts)
-                        local pair = opts.line:sub(opts.col - 1, opts.col)
-                        return vim.tbl_contains({
-                            brackets[1][1] .. brackets[1][2],
-                            brackets[2][1] .. brackets[2][2],
-                            brackets[3][1] .. brackets[3][2],
-                        }, pair)
-                    end)
-            }
-            for _, bracket in pairs(brackets) do
-                autopairs.add_rules {
-                    rule(bracket[1] .. ' ', ' ' .. bracket[2])
-                        :with_pair(function() return false end)
-                        :with_move(function(opts)
-                            return opts.prev_char:match('.%' .. bracket[2]) ~= nil
-                        end)
-                        :use_key(bracket[2])
-                }
-            end
-        end
-    },
-    -- TODO: check for the existence of alternatives
-    {
-        "tpope/vim-surround", -- Manipulate brackets and other delimiters
     },
     {
-        "tpope/vim-repeat", -- More advanced repeating with dot
+        "kylechui/nvim-surround",
+        version = "*",
+        config = true,
+        event = "VeryLazy", -- check
     },
     {
+        -- WARN: tastes have chaged, try to replicate Emacs for lisp development
+        -- A good starting point might be nvlime or vlime
         "gpanders/nvim-parinfer", -- Manages parenthesis while editing LISP-like files
         cond = false,
         ft = { "yuck", "lisp" }
