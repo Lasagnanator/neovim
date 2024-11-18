a256139b70617a97f3a92dad8fe36e34202d6314
Lasagnanator <d96a07@gmail.com>
2024-11-19 00:51:56 +0100

    Removed Ocaml tools that are installed with own package manager

--- lua/langs/ocaml.lua
+++ lua/langs/ocaml.lua
@@ -3,7 +3,7 @@ if not Langs.ocaml then return {} end
 local utils = require("core.utils")
 
 Treesitter:update({ "ocaml", "ocaml_interface" })
-Mason:update({ "ocaml-lsp", "ocamlearlybird", "ocamlformat" })
+Mason:update("ocamlearlybird")
 After:add(function ()
     require("lspconfig").ocamllsp.setup({
         on_attach = utils.on_attach,
