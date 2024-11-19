02c8255a7b4469b34d24901be92c50c4ad629042
Lasagnanator <d96a07@gmail.com>
2024-11-20 00:13:41 +0100

    Added remote-containers plugin for devcontainer support

---
+++ lua/plugins/remote-containers.lua
@@ -0,0 +1,7 @@
+-- NOTE: in case this plugin is not sufficient try amitds1997/remote-nvim.nvim
+-- keep in mind that it requires devpod for devcontainers
+
+---@type LazyPluginSpec
+return {
+    "jamestthompson3/nvim-remote-containers"
+}
