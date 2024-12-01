d86c0f49c9e8b8da7aa7cb6908f67f2206999ee8
Lasagnanator <d96a07@gmail.com>
2024-12-01 22:28:41 +0100

    Removed Tailwind LSP config since the plugin already configures it

--- lua/langs/tailwind.lua
+++ lua/langs/tailwind.lua
@@ -4,12 +4,6 @@ if not Langs.tailwind.enabled then return {} end
 local utils = require("core.utils")
 
 Mason:update("tailwindcss-language-server")
-After:add(function()
-    require("lspconfig").tailwindcss.setup({
-        on_attach = utils.on_attach,
-        capabilities = utils.set_capabilities()
-    })
-end)
 
 return {
     "luckasRanarison/tailwind-tools.nvim",
