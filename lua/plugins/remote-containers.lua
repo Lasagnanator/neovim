e6e9e1a16229b497c21fe1b18ef15ef9b0bb23bf
Lasagnanator <d96a07@gmail.com>
2024-12-02 00:17:20 +0100

    Removed remote-containers, just use Devpod

--- lua/plugins/remote-containers.lua
+++
@@ -1,7 +0,0 @@
--- NOTE: in case this plugin is not sufficient try amitds1997/remote-nvim.nvim
--- keep in mind that it requires devpod for devcontainers
-
----@type LazyPluginSpec
-return {
-    "jamestthompson3/nvim-remote-containers"
-}
