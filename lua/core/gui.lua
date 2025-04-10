95fdaec6b5fdb44eb1bb6ae7b6b698f9339d28a1
Lasagnanator <d96a07@gmail.com>
2025-04-10 11:03:43 +0200

    Added Jetbrains Mono as Windows GUI font

--- lua/core/gui.lua
+++ lua/core/gui.lua
@@ -1,5 +1,11 @@
 local settings = {
-    guifont = "JetBrains Mono:h11"
+    guifont = (function ()
+        if vim.fn.has("win32") then
+            return "JetBrainsMono Nerd Font Propo:h11"
+        else
+            return "JetBrains Mono:h11"
+        end
+    end)()
 }
 for k, v in pairs(settings) do
     vim.opt[k] = v
