79cb1cc89571666d40870236120c56a86c9675e4
Lasagnanator <d96a07@gmail.com>
2025-04-17 01:15:08 +0200

    Added Elixir configuration

---
+++ after/ftplugin/elixir.lua
@@ -0,0 +1,2 @@
+--<< Keybinds
+if Langs.elixir.enabled then require("keybinds.filetypes.elixir"):bufset() end
--- lua/langs/elixir.lua
+++ lua/langs/elixir.lua
@@ -1,5 +1,27 @@
 if not Langs.elixir.enabled then return {} end
 
--- TODO: configure
+local utils = require("core.utils")
 
-return {}
+Treesitter:update("elixir")
+
+return {
+    {
+        "elixir-tools/elixir-tools.nvim",
+        version = "*",
+        ft = "elixir",
+        config = function()
+            require("elixir").setup {
+                nextls = { enable = false },
+                enable = true,
+                elixirls = {
+                    enable = true,
+                    on_attach = utils.on_attach,
+                },
+                projectionist = { enable = false }
+            }
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    }
+}
