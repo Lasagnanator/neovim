d6a1f1a473606c5ffe3326d9d95072509fddeb8a
Lasagnanator <d96a07@gmail.com>
2025-10-02 00:28:39 +0200

    Added LSPConfig plugin

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+return {
+    {
+        -- NOTE: check the new way to call an LSP configuration and adapt the language modules
+        "neovim/nvim-lspconfig",
+        version = "*"
+    },
+}
