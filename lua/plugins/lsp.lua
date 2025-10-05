03be7df127a881a4e260a3465165179103845604
Lasagnanator <d96a07@gmail.com>
2025-10-05 19:21:18 +0200

    Removed redundant bracket on LSP configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,6 @@
 ---@type LazySpec
 return {
-    {
-        -- NOTE: check the new way to call an LSP configuration and adapt the language modules
-        "neovim/nvim-lspconfig",
-        version = "*"
-    },
+    -- NOTE: check the new way to call an LSP configuration and adapt the language modules
+    "neovim/nvim-lspconfig",
+    version = "*"
 }
