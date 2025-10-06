3e79537e9f153a80ff131b7ce458726a816f9fb3
Lasagnanator <d96a07@gmail.com>
2025-10-06 23:57:47 +0200

    Migrated lspconfig and lua to new LSP API

--- lua/langs/lua.lua
+++ lua/langs/lua.lua
@@ -1,25 +1,22 @@
 if not Langs.lua then return {} end
 
-local utils = require("core.utils")
-
 Treesitter:update("lua")
 Mason:update("lua-language-server")
-After:add(function()
-    require("lspconfig").lua_ls.setup({
-        on_attach = utils.on_attach,
-        capabilities = utils.set_capabilities(),
-        settings = {
-            Lua = {
-                telemetry = {
-                    enable = false,
-                },
-                hint = {
-                    enable = false,
-                }
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            telemetry = {
+                enable = false,
             },
+            hint = {
+                enable = false,
+            }
         },
-    })
-end)
+    },
+})
+
+vim.lsp.enable("lua_ls")
 
 ---@type LazySpec
 return {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,5 +2,13 @@
 return {
     -- NOTE: check the new way to call an LSP configuration and adapt the language modules
     "neovim/nvim-lspconfig",
-    version = "*"
+    version = "*",
+    config = function ()
+        local utils = require("core.utils")
+        require("lspconfig")
+        vim.lsp.config("*", {
+            on_attach = utils.on_attach,
+            capabilities = utils.set_capabilities(),
+        })
+    end
 }
