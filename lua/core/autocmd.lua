8260eb6250192a6c69744f8816cc197cf5ee827e
Lasagnanator <d96a07@gmail.com>
2025-10-06 23:57:23 +0200

    Added description and augroups to autocommands

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -2,16 +2,24 @@
 
 --<< Vars
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 --<< Autocommands
 autocmd("TextYankPost", {
+    desc = "Highlight yanked text",
+    group = augroup("highlight_yank", { clear = true }),
     callback = function()
         vim.cmd("silent!")
         vim.highlight.on_yank({ timeout = 150 })
     end
 })
 
+
+local au_teminal = augroup('terminal_buf', { clear = true })
+
 autocmd("TermOpen", {
+    desc = "Set specific options for terminal buffers",
+    group = au_teminal,
     pattern = "term://*",
     callback = function()
         vim.opt_local["number"] = false
@@ -21,6 +29,8 @@ autocmd("TermOpen", {
 })
 
 autocmd("BufEnter", {
+    desc = "Start insert mode when entering a terminal buffer",
+    group = au_teminal,
     pattern = "term://*",
     callback = function()
         vim.cmd("startinsert")
@@ -28,6 +38,8 @@ autocmd("BufEnter", {
 })
 
 autocmd("BufLeave", {
+    desc = "Stop insert mode when exiting a terminal buffer",
+    group = au_teminal,
     pattern = "term://*",
     callback = function()
         vim.cmd("stopinsert")
