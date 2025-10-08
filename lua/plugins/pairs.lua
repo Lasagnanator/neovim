0bc445b7a131cc23aa30c85305dc7872baeb5663
Lasagnanator <d96a07@gmail.com>
2025-10-08 18:21:20 +0200

    Cleaned and described custom pairs function

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -12,36 +12,44 @@ return {
             enable_check_bracket_line = false,
         },
         config = function(_, opts)
-            local npairs = require("nvim-autopairs")
+            local autopairs = require("nvim-autopairs")
             local Rule = require("nvim-autopairs.rule")
-            local cond = require("nvim-autopairs.conds")
 
-            npairs.setup(opts)
+            -- Base setup
+            autopairs.setup(opts)
 
-            local function create_rule(a1, ins, a2, lang)
-                npairs.add_rule(
+            ---Create a custom rule for inserting specific symbols between brackets
+            ---@param bra string Left symbol
+            ---@param ins string Symbol to insert
+            ---@param ket string Right symbol
+            ---@param lang? string Language for conditional activation of rule
+            local function create_rule(bra, ins, ket, lang)
+                autopairs.add_rule(
                     Rule(ins, ins, lang)
-                    :with_pair(function(options)
-                        return a1 .. a2 ==
-                            options.line:sub(options.col - #a1, options.col + #a2 - 1)
+                    :with_pair(function(pos)
+                        return bra .. ket ==
+                            pos.line:sub(pos.col - #bra, pos.col + #ket - 1)
                     end)
-                    :with_move(cond.none())
-                    :with_cr(cond.none())
-                    :with_del(function(options)
+                    :with_del(function(pos)
                         local col = vim.api.nvim_win_get_cursor(0)[2]
-                        return a1 .. ins .. ins .. a2 ==
-                            options.line:sub(col - #a1 - #ins + 1, col + #ins + #a2) -- insert only works for #ins == 1 anyway
+                        return bra .. ins .. ins .. ket ==
+                            pos.line:sub(col - #bra - #ins + 1, col + #ins + #ket)
                     end)
                 )
             end
 
+            -- Space between brackets
             create_rule('(', ' ', ')')
             create_rule('[', ' ', ']')
             create_rule('{', ' ', '}')
 
+            -- Jinja brackets
             create_rule('{', '%', '}')
             create_rule('{%', ' ', '%}')
+            create_rule('{', '#', '}')
+            create_rule('{#', ' ', '#}')
 
+            -- Ocaml
             create_rule('(', '*', ')', 'ocaml')
             create_rule('(*', ' ', '*)', 'ocaml')
         end,
