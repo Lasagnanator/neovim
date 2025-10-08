2efcc3574362e3eb7cef3c9c6031d6520c0dbefd
Lasagnanator <d96a07@gmail.com>
2025-10-09 00:33:09 +0200

    Added fzf-lua and nvim-web-devicons

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+	"ibhagwan/fzf-lua",
+	opts = {},
+	keys = require("keybinds.plugins.fzf"):to_lazy(),
+	init = function()
+		require("fzf-lua").register_ui_select()
+	end,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+}
