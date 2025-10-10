48723860b9c89f600e35e3031993ba9460f2d452
Lasagnanator <d96a07@gmail.com>
2025-10-10 17:46:40 +0200

    Added empty linters table on start to delete defaults

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,6 +2,7 @@
 return {
     "mfussenegger/nvim-lint",
     config = function()
+        require("lint").linters_by_ft = vim.empty_dict()
         vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost" }, {
             callback = function()
                 require("lint").try_lint()
