df0e7f70b7c140627a26c8f077d429b95282df03
Lasagnanator <d96a07@gmail.com>
2025-10-10 17:47:56 +0200

    First iteration of new Mason autoinstall logic

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,22 @@
+---@type LazySpec
 return {
     {
         "williamboman/mason.nvim",
         opts = { registries = { "github:mason-org/mason-registry" } },
-        init = function() require("keybinds.plugins.mason"):set() end
+        keys = require("keybinds.plugins.mason"):to_lazy(),
+        lazy = false,
     },
     {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         opts = { ensure_installed = Mason.tools },
-        dependencies = { "williamboman/mason.nvim" }
+        dependencies = { "williamboman/mason.nvim" },
+        init = function ()
+            vim.api.nvim_create_augroup("mason", { clear = true })
+            vim.api.nvim_create_autocmd("VimEnter", {
+                desc = "Install missing tools with Mason",
+                group = "mason",
+                command = "MasonToolsInstall"
+            })
+        end
     },
 }
