8d1f0b42cd7be2fcdea18aa00f6113161f070a65
Lasagnanator <d96a07@gmail.com>
2025-10-20 17:31:39 +0200

    Added auto-enable of treesitter highlight and indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,23 @@ return {
 		local ts = require("nvim-treesitter")
 		ts.setup()
 		ts.install(Treesitter.parsers)
+		local au_treesitter = vim.api.nvim_create_augroup("treesitter", { clear = true })
+		vim.api.nvim_create_autocmd("Filetype", {
+			desc = "Start treesitter functions",
+			group = au_treesitter,
+			callback = function(args)
+                local buf = args.buf
+                local filetype = args.match
+                local language = vim.treesitter.language.get_lang(filetype) or filetype
+                if not vim.treesitter.language.add(language) then
+                    return
+                end
+                -- Indent
+				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                -- Highlight
+				vim.treesitter.start()
+			end,
+		})
 	end,
 	build = ":TSUpdate",
 	lazy = false,
