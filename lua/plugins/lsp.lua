e162e91c61df7ef7e95d4435a1619c8fc7e486a6
Lasagnanator <d96a07@gmail.com>
2025-10-20 17:32:14 +0200

    LSPConfig format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,13 @@
 ---@type LazySpec
 return {
-    -- NOTE: check the new way to call an LSP configuration and adapt the language modules
-    "neovim/nvim-lspconfig",
-    version = "*",
-    config = function ()
-        local utils = require("core.utils")
-        require("lspconfig")
-        vim.lsp.config("*", {
-            on_attach = utils.on_attach,
-            capabilities = utils.set_capabilities(),
-        })
-    end
+	"neovim/nvim-lspconfig",
+	version = "*",
+	config = function()
+		local utils = require("core.utils")
+		require("lspconfig")
+		vim.lsp.config("*", {
+			on_attach = utils.on_attach,
+			capabilities = utils.set_capabilities(),
+		})
+	end,
 }
