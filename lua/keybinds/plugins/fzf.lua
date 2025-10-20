152ce6cb6ef8cf7c0cb87871f5f8de9a9dce2820
Lasagnanator <d96a07@gmail.com>
2025-10-20 17:34:11 +0200

    Added help and buffers keybinds to fzf

--- lua/keybinds/plugins/fzf.lua
+++ lua/keybinds/plugins/fzf.lua
@@ -3,6 +3,8 @@ local keys   = require("core.classes").Keybinds_group
 local silent = { noremap = true, silent = true }
 
 return keys:new({
-    key:new("n", "<Leader>ff", function () require("fzf-lua").files() end, "Files", silent),
-    key:new("n", "<Leader>fw", function () require("fzf-lua").live_grep() end, "Live grep", silent),
+    key:new("n", "<Leader>ff", function() require("fzf-lua").files() end, "Files", silent),
+    key:new("n", "<Leader>fw", function() require("fzf-lua").live_grep() end, "Live grep", silent),
+    key:new("n", "<Leader>fk", function() require("fzf-lua").buffers() end, "Buffers", silent),
+    key:new("n", "<Leader>f?", function() require("fzf-lua").helptags() end, "Buffers", silent),
 })
