4a7d506daac8f02ee50ae06e92bbf0c6c42e3b7f
Lasagnanator <d96a07@gmail.com>
2025-10-21 17:44:34 +0200

    Added Gitsigns

---
+++ lua/keybinds/plugins/gitsigns.lua
@@ -0,0 +1,15 @@
+local key = require("core.classes").Keybind
+local keys = require("core.classes").Keybinds_group
+local silent   = { noremap = true, silent = true }
+
+return keys:new({
+    key:new("n", "<Leader>gb", "<CMD>Gitsigns toggle_current_line_blame<CR>", "Toggle line blame", silent),
+    key:new("n", "<Leader>gr", "<CMD>Gitsigns reset_hunk<CR>",                "Reset hunk", silent),
+    key:new("n", "<Leader>gs", "<CMD>Gitsigns select_hunk<CR>",               "Select hunk", silent),
+    key:new("n", "<Leader>gi", "<CMD>Gitsigns toggle_signs<CR>",              "Toggle signs", silent),
+    key:new("n", "<Leader>gn", "<CMD>Gitsigns toggle_numhl<CR>",              "Toggle number highlight", silent),
+    key:new("n", "<Leader>gl", "<CMD>Gitsigns toggle_linehl<CR>",             "Toggle line highlight", silent),
+    key:new("n", "<Leader>ge", "<CMD>Gitsigns toggle_deleted<CR>",            "Toggle deleted", silent),
+    key:new("n", "]c",         "<CMD>Gitsigns next_hunk<CR>",                 "Hunk", silent),
+    key:new("n", "[c",         "<CMD>Gitsigns prev_hunk<CR>",                 "Hunk", silent),
+})
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,21 @@
+---@type LazySpec
+return {
+	"lewis6991/gitsigns.nvim",
+	branch = "main",
+	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "-" },
+			topdelete = { text = "-" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signcolumn = true,
+		numhl = false,
+		linehl = false,
+		word_diff = false,
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	keys = require("keybinds.plugins.gitsigns"):to_lazy(),
+}
