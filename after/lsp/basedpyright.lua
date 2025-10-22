dfbc7ba7f39bcbae51226975c47b03e58aabd2c8
Lasagnanator <d96a07@gmail.com>
2025-10-23 00:49:53 +0200

    Moved lsp folder to after for Python

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -1,4 +1,6 @@
 return {
+    on_attach = require("core.utils").on_attach,
+    capabilities = require("core.utils").set_capabilities(),
 	settings = {
 		basedpyright = {
 			analysis = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ return {
 	version = "*",
 	config = function()
 		local utils = require("core.utils")
-		require("lspconfig")
 		vim.lsp.config("*", {
 			on_attach = utils.on_attach,
 			capabilities = utils.set_capabilities(),
