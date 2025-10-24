363327f7ad9f663eae4db779201c44a3d3d06f8e
Lasagnanator <d96a07@gmail.com>
2025-10-25 00:30:21 +0200

    Changed Flutter plugin configuration

--- lua/plugins/langs/flutter.lua
+++ lua/plugins/langs/flutter.lua
@@ -1,8 +1,28 @@
 if not Langs.flutter then return {} end
 
+---@type LazySpec
 return {
     "nvim-flutter/flutter-tools.nvim",
-    config = true,
+    opts = {
+        fvm = true,
+        decoration = {
+            statusline = {
+                -- TODO: configure lualine with this fields
+                app_version = true,
+                device = true,
+            },
+        },
+        debugger = {
+            enabled = true,
+        },
+        -- TODO: test
+        widget_guides = {
+            enabled = true,
+        },
+        settings = {
+            showTodos = false,
+        },
+    },
     ft = "dart",
     dependencies = {
         "nvim-lua/plenary.nvim",
