99e790921a6b5283677b20d1a99db969e1c0cd18
Andrea Divo <andrea.divo@innovatrieste.it>
2026-05-11 15:28:49 +0000

    feat(lsp): progress

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -3,6 +3,7 @@ vim.lsp.inlay_hint.enable(true)
 
 --<< Autocommand
 local au_lsp = vim.api.nvim_create_augroup("lsp", { clear = true })
+
 vim.api.nvim_create_autocmd("LspAttach", {
     desc = "Attach keybinds and set options when an LSP attacches to a buffer",
     group = au_lsp,
@@ -11,4 +12,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
         require("keybinds.plugins.lsp").on_attach(bufopts):set()
     end,
+})
+
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(ev)
+    local value = ev.data.params.value
+    vim.api.nvim_echo({ { value.message or "done" } }, false, {
+      id = "lsp." .. ev.data.client_id,
+      kind = "progress",
+      source = "vim.lsp",
+      title = value.title,
+      status = value.kind ~= "end" and "running" or "success",
+      percent = value.percentage,
+    })
+  end,
 })
