6db0dff51fc2cd1229516e923a11862a5db8eff8
Lasagnanator <d96a07@gmail.com>
2026-05-19 01:35:02 +0200

    feat(puppet): linter and formatter

--- after/plugin/puppet.lua
+++ after/plugin/puppet.lua
@@ -1,1 +1,5 @@
-if Langs.puppet then vim.lsp.enable("puppet") end
+if Langs.puppet then
+    vim.lsp.enable("puppet")
+    require("lint").linters_by_ft.puppet = { "puppet-lint" }
+    require("conform").formatters_by_ft.puppet = { "puppet-lint" }
+end
