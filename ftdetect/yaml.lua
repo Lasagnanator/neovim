bd271e3defcea5e9de6a71eff100e23b5acc4d46
Lasagnanator <d96a07@gmail.com>
2026-05-23 11:34:22 +0200

    feat(yaml): added network-config to detected filenames

--- ftdetect/yaml.lua
+++ ftdetect/yaml.lua
@@ -2,6 +2,7 @@ vim.filetype.add({
     filename = {
         ["user-data"] = "yaml",
         ["meta-data"] = "yaml",
+        ["network-config"] = "yaml",
         ["vendor-data"] = "yaml",
     },
 })
