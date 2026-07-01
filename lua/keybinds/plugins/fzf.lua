ba1baeb7bcbf4f4ba72dbe3fc04791f759c23365
Andrea Divo <andrea.divo@innovatrieste.it>
2026-07-01 08:51:56 +0000

    fix(fzf): diagnostics keybinds description

--- lua/keybinds/plugins/fzf.lua
+++ lua/keybinds/plugins/fzf.lua
@@ -3,8 +3,8 @@ local keys = require("core.classes").Keybinds_group
 local silent = { noremap = true, silent = true }
 
 return keys:new({
-    key:new("n", "<Leader>fd", function() require("fzf-lua").diagnostics_document() end, "Files", silent),
-    key:new("n", "<Leader>fD", function() require("fzf-lua").diagnostics_workspace() end, "Files", silent),
+    key:new("n", "<Leader>fd", function() require("fzf-lua").diagnostics_document() end, "Local diagnostics", silent),
+    key:new("n", "<Leader>fD", function() require("fzf-lua").diagnostics_workspace() end, "Workspace diagnostics", silent),
     key:new("n", "<Leader>ff", function() require("fzf-lua").files() end, "Files", silent),
     key:new("n", "<Leader>fk", function() require("fzf-lua").buffers() end, "Buffers", silent),
     key:new("n", "<Leader>fw", function() require("fzf-lua").live_grep() end, "Live grep", silent),
